8a2edcbb1054e8f67d350d884ec640616cd5d625
Anthony Gasca <ag@theblacksea.cc>
2013-01-14 02:22:14 -0700

    t

--- public/js/bus.js
+++ public/js/bus.js
@@ -10,6 +10,7 @@
 		var socket = io.connect("http://204.27.60.58");
 		
 		socket.on('*', function (paramArray) {
+			console.log(paramArray);
 			var module     = paramArray[0]
 			, method       = paramArray[1]
 			, args         = paramArray[2]; // msgpacked object
