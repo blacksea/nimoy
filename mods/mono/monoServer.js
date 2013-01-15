7086f45de6c9ef93a305aad537752d99290a32ec
Anthony Gasca <ag@theblacksea.cc>
2013-01-15 11:17:03 -0700

    pruned public and mods

--- mods/mono/monoServer.js
+++
@@ -1,1 +0,0 @@
-// MONOME CLASS::SERVER 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,24 @@
+
 // m o n o m e
+
 (function (window) {
 	var Monome = function (template) {
+		
 		var monome = this;
 		monome.template = template;
-
 		// extend a basic uipanel construct new instance of panel 
 		// and populate with needed elements
-
 		monome.init = function () {
 			monome.render();
 		}
-		monome.render = function () {
-			var html = templayed(monome.template)({test:''});
-			var container = document.createElement('div');
-			container.innerHTML = html;
-			document.body.appendChild(container);
-			container.onClick = function () {
-				console.log('test click monome!!!');
-			}
-			monome.ready();
+		monome.renderUI = function () {
+			
 		}
-
-		monome.set = function (row, min, max, vel) {
-			bus.send(['mono','set','skeleton','log',[row,min,max,vel]]);
-		}
-
+		// make nested micro panels with mapped interface / ui elements
+		var panel = new Ui.panel({
+		});
+		// replace ui with uikit instances
 		monome.ready = function () {
-			console.log('monome ready!');
 			var box = document.getElementById('monome')
 			, num   = box.querySelector('.number')
 			, offX  = 0
@@ -36,26 +28,16 @@
 				offY = event.clientY - box.offsetTop;
 			}
 			box.ondrag = function (event) {
-				box.style.top = (event.clientY-offY)+'px';
 				box.style.left = (event.clientX-offX)+'px';
+				box.style.top = (event.clientY-offY)+'px';
 			}
 			box.ondragend = function (event) {
 				event.preventDefault();
 			}
-			// grip.onmousedown = function () {
-			// 	console.log('test click monome!!!');
-			// }
-			num.onmousewheel = function (event) {
-				var value = event.wheelDeltaY;
-				var oldVal = parseInt(this.innerHTML);
-				var newVal = null;
-				if(value<0){
-					newVal = oldVal+Math.abs(value);
-				}else{
-					newVal = oldVal-Math.abs(value);
-				}
-				this.innerHTML = newVal;
-			}
+		}
+		// server interface
+		monome.set = function (row, min, max, vel) {
+			bus.send(['mono','set','skeleton','log',[row,min,max,vel]]);
 		}
 	}
 	window.mono = Monome;
@@ -102,23 +84,47 @@
 }(window));
 // U I  K I T
 
-// -- shape shifter -- //
+// figure out how to do testing 
+// finish this class
 
+// -- shape shifter -- //
 (function (window) {
 
 	var Ui = function (template) {
 
 		var ui = this;
 		ui.template = template;
+		// construct different components
+		// events 
+		// for sliders and numboxes
+		num.onmousewheel = function (event) {
+			var oldVal = parseInt(this.innerHTML)
+			, value = event.wheelDeltaY
+			, newVal = null;
+			if(value<0){
+				newVal = oldVal+Math.abs(value);
+			}else{
+				newVal = oldVal-Math.abs(value);
+			}
+			this.innerHTML = newVal;
+		}
+		// for draggable components / panels
+		ondragstart = function () { 
 
+		}
+		ondragend = function () {
+			
+		}
+		ondrag = function () {
+
+		}
+		// class to route to - // overide 
 		// provide structure and assemble
 		var templates = document.createElement('div');
 		templates.innerHTML = ui.template;
-		
 		ui.init = function () {
 			console.log(ui.getComponent('panel'));
 		}
-
 		ui.getComponent = function (component) {
 			var elements = templates.getElementsByTagName(component);
 			return elements[0].innerHTML; 
