9bdb07c69635086d29560eb03816b2d58a48fad9
Anthony Gasca <ag@theblacksea.cc>
2013-01-27 08:13:04 -0700

    updated bus class, now available to all modules

--- mods/bus.js
+++ mods/bus.js
@@ -2,21 +2,21 @@
 // C L I E N T  B U S
 
 (function (window) {
-	
-	var Bus = function () {
-		
-		var socket = io.connect("http://127.0.0.1:8888");
-		
-		socket.on('*', function (paramArray) {
-			var module = paramArray[0]
-			, method   = paramArray[1]
-			, args     = paramArray[2];
-			window[module][method](args);
-		});
 
-		this.send = function (paramArray) {
-			socket.emit('*', paramArray);
-		}
+	var socket = io.connect("http://127.0.0.1:8888");
+
+	socket.on('*', function (paramArray) {
+		console.log(paramArray);
+		var module = paramArray[0]
+		, method   = paramArray[1]
+		, args     = paramArray[2];
+		window[module][method](args);
+	});
+
+	var Bus = function () {}
+
+	Bus.send = function (paramArray) {
+		socket.emit('*', paramArray);
 	}
 
 	window.bus = Bus;
