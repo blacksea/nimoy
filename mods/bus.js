a5f1189d1810a0388b4e54d95edc45290b049a4c
Anthony Gasca <ag@theblacksea.cc>
2013-02-04 12:42:14 -0700

    replace shoe with straightup sockJS

--- mods/bus.js
+++ mods/bus.js
@@ -1,5 +1,7 @@
 // C L I E N T  B U S
-
+var shoe = require('shoe')
+, MuxDemux = require('mux-demux');
+var md = new MuxDemux;
 (function (window) {
 	var Bus = function () {}
 	var socket = io.connect("http://127.0.0.1:8888");
