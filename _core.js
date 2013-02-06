95a3e61bedfcde1ac4e8d89acb2e23d505ad3077
Anthony Gasca <ag@theblacksea.cc>
2013-02-06 14:40:21 -0700

    removed _core_server.js

--- _core.js
+++ _core.js
@@ -1,7 +1,6 @@
 /* C O R E 
-	app setup
+    app setup
 */
-
 var shell = require('./_shell')
 , brico = require('./_brico')
 , connect = require('connect')
@@ -9,13 +8,14 @@ var shell = require('./_shell')
 , browserify = require('browserify')
 , shoe = require('shoe')
 , fs = require('fs')
+, filed = require('filed')
 , uglifyJS = require('uglify-js');
 
 // bundle browser side code
-var bundle = browserify('./_core_client.js').bundle();
+var bundle = browserify('./_components/core.js').bundle();
 var bundleMin = uglifyJS.minify(bundle, {fromString:true});
 fs.writeFile('./_components/public/bundle.min.js', bundleMin.code, function(err) {
-		if (err) console.log(err);
+        if (err) console.log(err);
 });
 
 var iron = new shell;
@@ -27,23 +27,18 @@ var app = connect()
 , server = require('http').createServer(app);
 
 app.use(function(req, res) { // handle http responses
-	res.setHeader("Content-Type", "text/html");
-	var stream = fs.createReadStream('_components/public/frame.html');
-  stream.on('open', function () {
-	  stream.pipe(res);
-	});
+    res.setHeader("Content-Type", "text/html");
+  filed('_components/public/frame.html').pipe(res);
 });
 
 server.listen(8888);
+console.log(shell);
 
 var sock = shoe(function (stream) {
-    var iv = setInterval(function () {
-        stream.write(Math.floor(Math.random() * 2));
-    }, 250);
-    stream.on('end', function () {
-        clearInterval(iv);
-    });
-    stream.pipe(process.stdout, { end : false });
+    //muxdemux here
+});
+sock.pipe(shell.bus).pipe(sock);
+sock.on('connection', function (conn) {
 });
 
 sock.install(server, '/bus');
