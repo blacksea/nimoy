2ab0dcb2b2fa634bb6cb97b998279ca89a8b0ccb
Anthony Gasca <ag@theblacksea.cc>
2013-02-06 19:29:47 -0700

    removed ecstatic for stock http

--- _core.js
+++ _core.js
@@ -4,24 +4,30 @@
 var shell = require('./_shell')
 , brico = require('./_brico')
 , http = require('http')
-, ecstatic = require('ecstatic')
 , browserify = require('browserify')
 , shoe = require('shoe')
 , fs = require('fs')
 , filed = require('filed')
 , uglifyJS = require('uglify-js');
-
-// bundle browser side code
+// bundle browser side code -- stick this in a module!!!!
 var bundle = browserify('./_components/core.js').bundle();
 var bundleMin = uglifyJS.minify(bundle, {fromString:true});
 fs.writeFile('./_components/public/bundle.min.js', bundleMin.code, function(err) {
-        if (err) console.log(err);
+    if (err) console.log(err);
 });
 var iron = new shell;
-ecstatic({ root: __dirname + '/public', handleError: false })
-
+var server = http.createServer(function (req, res) {
+    if(req.url==='/bundle.min.js'){
+        filed('_components/public/bundle.min.js').pipe(res);
+    }
+    else if (req.url==='/'){
+        filed('_components/public/frame.html').pipe(res);
+    }
+    else {
+        res.end('fuuuuuuk');
+    }
+});
 server.listen(8888);
-
 // setup socket connection
 var sock = shoe(iron.Stream);
 sock.on('connection', iron.Conn); // create streams now
