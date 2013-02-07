e73fe8e3615e324dbf4658ab0ac742a3596b2c3c
Anthony Gasca <ag@theblacksea.cc>
2013-02-06 17:56:04 -0700

    removing connect

--- _core.js
+++ _core.js
@@ -3,14 +3,12 @@
 */
 var shell = require('./_shell')
 , brico = require('./_brico')
-, connect = require('connect')
 , http = require('http')
+, ecstatic = require('ecstatic')
 , browserify = require('browserify')
 , shoe = require('shoe')
 , fs = require('fs')
 , filed = require('filed')
-, Stream = require('stream')
-, MuxDemux = require('mux-demux')
 , uglifyJS = require('uglify-js');
 
 // bundle browser side code
@@ -19,37 +17,12 @@ var bundleMin = uglifyJS.minify(bundle, {fromString:true});
 fs.writeFile('./_components/public/bundle.min.js', bundleMin.code, function(err) {
         if (err) console.log(err);
 });
-
 var iron = new shell;
-
-var app = connect()
-.use(connect.logger('dev'))
-.use(connect.compress())
-.use(connect.static('_components/public'))
-, server = require('http').createServer(app);
-
-app.use(function(req, res) { // handle http responses
-    res.setHeader("Content-Type", "text/html");
-  filed('_components/public/frame.html').pipe(res);
-});
+ecstatic({ root: __dirname + '/public', handleError: false })
 
 server.listen(8888);
 
-var mx = MuxDemux();
-mx.on('connection', function (stream) {
-    stream.on('data', function (data) {
-        console.log(stream.meta + ' ' +data);
-    });
-});
-var sock = shoe(function (stream) {
-    mx.pipe(stream).pipe(mx);    
-});
-sock.on('connection', function (conn) {
-    console.log(conn);
-    var ds = mx.createStream('ff');
-    setInterval(function() {
-        ds.write('goondoor');
-    }, 50); 
-});
-
+// setup socket connection
+var sock = shoe(iron.Stream);
+sock.on('connection', iron.Conn); // create streams now
 sock.install(server, '/bus');
--- _shell.js
+++ _shell.js
@@ -8,6 +8,19 @@ var MuxDemux = require('mux-demux');
 
 module.exports = function () {
 	var self = this;
+	// setup sock conn.
+	self.bus = MuxDemux();
+	self.bus.on('connection', function (stream) {
+	    stream.on('data', function (data) {
+	        console.log(stream.meta + ' ' +data);
+	    });
+	});
+	self.Stream = function (stream) {
+		self.bus.pipe(stream).pipe(self.bus);		
+	}
+	self.Conn = function (conn) {
+
+	}
 	self.create = function (component) {
 	}
 	self.destroy = function (component) {
