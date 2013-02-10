b636209bda22c2e8afff84f836ae7e14a0fb6519
Anthony Gasca <ag@theblacksea.cc>
2013-02-10 12:01:42 -0700

    updated routes

--- _route.js
+++ _route.js
@@ -4,9 +4,9 @@
 var filed = require('filed');
 module.exports = function (req, res) {
   if (req.url==='/bundle.min.js') {
-    filed('_components/public/bundle.min.js').pipe(res);
+    filed('_wilds/bundle.min.js').pipe(res);
   } else if (req.url==='/') { 
-    filed('_components/public/frame.html').pipe(res);
+    filed('_wilds/frame.html').pipe(res);
   } else {
     res.end('fuuuuuuk');
    }
