9d59f04f74bfbee9d5e7dd54d1324024aae4d7a7
Anthony Gasca <ag@theblacksea.cc>
2013-02-17 17:10:12 -0700

    update _brico for streams2 api

--- _brico.js
+++ _brico.js
@@ -9,7 +9,7 @@ module.exports = function (dir) {
   /////////////////////////
   this.stream = MuxDemux();
   this.stream.on('connection', function (stream) {
-    stream.on('data', function (data) { // hook / pipe stream from here
+    stream.on('readeable', function (data) { // hook / pipe stream from here
       // console.log(stream.meta+' '+fillibuster);
     });
   /////////////////////////
