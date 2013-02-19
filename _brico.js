4fd65484b750ed17f07acfb4e025c0842232c18e
Anthony Gasca <ag@theblacksea.cc>
2013-02-18 22:15:55 -0700

    _data.js moved to _wilds, now implemented as a module

--- _brico.js
+++ _brico.js
@@ -6,12 +6,12 @@ module.exports = function (dir) {
   var self = this;
   this.init = function (map) { // an array of objs 
   }
-  /////////////////////////
+  ////////////////////////////////////////////////
   this.stream = MuxDemux();
   this.stream.on('connection', function (stream) {
     stream.on('data', function (data) { // hook / pipe stream from here
        console.log(stream.meta+' '+data);
     });
-  /////////////////////////
+  /////////////////////////////////////////////////////////////////////
   });
 }
---
+++ _wilds/data.js
@@ -0,0 +1,33 @@
+/*{
+  "id":"data",
+	"scope":["server"],
+	"desc":"data(redis) api/interface"
+}*/
+var redis = require('redis')
+, client = redis.createClient();
+// an easy api to store retreive data // map filter
+module.exports = function () {
+  var self = this;
+  this.set = function (params) {
+    var key = params[0]
+    , val = params[1];
+    client.set(key, val);
+  }
+  this.get = function (key, err, cb) {
+    client.get(key, cb);
+  }
+  this.hset = function (params) {
+    var hash = params[0]
+    , key = params[1]
+    , val = params[2];
+    client.hset(hash, key, val);
+  }
+  this.hget = function (prams, cb) {
+    var hash = params[0]
+    , key = params[1]
+    , val = params[2];
+    client.hget(params, cb);
+  }
+  // support for lists / hashes ????!!!
+  // data implementation -------- 
+}  
