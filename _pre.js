837ba6d99c919dfdaa55f3592548717d1bf0c4ed
blacksea <ag@theblacksea.cc>
2013-04-09 13:09:50 -0600

    fixed _pre

--- _pre.js
+++ _pre.js
@@ -6,55 +6,42 @@ var browserify = require('browserify')
 , uglifyJS = require('uglify-js')
 
 exports = function (stream) {
+  var _ = this,
 
   stream.on('data', function (data) {
 
-  }
+  })
 
   stream.on('end', function () {
 
+  })
+
+  browserify(_.JS).bundle({}, compile)
+
+  function compile (bundle) {
+    var JS = opts.js
+
+    if(opts.compress===true) {
+      var bundleMin = uglifyJS.minify(bundle,{fromString: true})
+      bundle = bundleMin.code
+    }
+
+    fs.writeFile(destJS,bundle,function (err) {
+      if (err) throw err
+      fs.readFile(opts.css, writeCSS)
+    })
   }
 
-
-
-}
-var Pre = function () {
-  var _ = this,
-
-  this.bundle = function (opts,cb) {
-    for (opt in opts) {
-      _[opt] = opts[opt]
-    }
-
-    browserify(_.JS).bundle({}, compile)
-
-    function compile (bundle) {
-      var JS = opts.js
-
-      if(opts.compress===true) {
-        var bundleMin = uglifyJS.minify(bundle,{fromString: true})
-        bundle = bundleMin.code
-      }
-
-      fs.writeFile(destJS,bundle,function (err) {
-        if (err) throw err
-        fs.readFile(opts.css, writeCSS)
-      })
-    }
-
-    function writeCSS (err, css) {
+  function writeCSS (err, css) {
+    if (err) throw err
+    var styles = data.toString(),
+    styles += self.css
+    stylus.render(styles, {filename:destCSS}, function (err, css) {
       if (err) throw err
-      var styles = data.toString(),
-      styles += self.css
-      stylus.render(styles, {filename:destCSS}, function (err, css) {
+      fs.writeFile(destCSS, css, function (err) {
         if (err) throw err
-        fs.writeFile(destCSS, css, function (err) {
-          if (err) throw err
-          cb();
-        })
+        cb();
       })
-    }
-  }    
+    })
+  }
 }
-
-module.exports = new Pre()
