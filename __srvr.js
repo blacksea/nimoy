f74d869a22904e03fc75a972039db007c43ec77c
blacksea <ag@theblacksea.cc>
2013-04-27 11:45:50 -0600

    added dumb port var

--- __srvr.js
+++ __srvr.js
@@ -7,6 +7,8 @@ var Bricoleur = require('./_brico')
 , http = require('http')
 , shoe = require('shoe')
 
+var port = 80 // set port
+
 var _usr = new usr()
 _usr.buildUsers(function (user) {
   Object['_'+user.name] = new Bricoleur(user) // not too sure about this prob a temp hack
@@ -18,7 +20,7 @@ var _pre = new pre({js:['./__clnt.js'],css:'./_wilds/_css',compress:true})
 _map.out.pipe(_pre.in)
 
 var _rtr = new rtr()
-var sock = shoe({log:'error'}, _rtr.handleData) // handle socket connections 
 var server = http.createServer(_rtr.handleReqs) // handle http requests
-server.listen(80)
+server.listen(port)
+var sock = shoe({log:'error'}, _rtr.handleData) // handle socket connections 
 sock.install(server, '/bus')
