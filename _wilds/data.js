b656fe6bb0fa8bd6af970f0ce43fff7670e07433
blacksea <ag@theblacksea.cc>
2013-05-05 19:57:48 -0600

    updated _wilds/data module

--- _wilds/data.js
+++ _wilds/data.js
@@ -4,35 +4,52 @@
 	"desc":"data(redis) api/interface"
 }*/
 
-var _ = Object._;
-
 var redis = require('redis')
-, client = redis.createClient();
+, client = redis.createClient()
+, telepath = require('tele')
 
 module.exports = function () {
-  var self = this;
+  var self = this
+  telepath(this)
+
+  this.recv = function (data) {
+    for (key in data) {
+      switch (key) {
+        case 'set': self.set(data[key]);break
+        case 'get': self.get(data[key]);break
+        case 'hset': self.hset(data[key]);break
+        case 'hget': self.hget(data[key]);break
+      }
+    }
+  }
 
   this.set = function (params) {
     var key = params[0]
-    , val = params[1];
-    client.set(key, val);
+    , val = params[1]
+    client.set(key, val)
   }
 
-  this.get = function (key, err, cb) {
-    client.get(key, cb);
+  this.get = function (key) {
+    client.get(key, function (err, data) {
+      if (err) throw err
+      console.log('data got '+data)
+      self.send(data)
+    })
   }
 
   this.hset = function (params) {
     var hash = params[0]
     , key = params[1]
-    , val = params[2];
-    client.hset(hash, key, val);
+    , val = params[2]
+
+    client.hset(hash, key, val)
   }
 
   this.hget = function (prams, cb) {
     var hash = params[0]
     , key = params[1]
-    , val = params[2];
-    client.hget(params, cb);
+    , val = params[2]
+
+    client.hget(params, cb)
   }
 }  
