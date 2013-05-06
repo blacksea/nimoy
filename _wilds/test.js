c5a04e636994b5bc89c7ad1ce98c0de08397d6a1
blacksea <ag@theblacksea.cc>
2013-05-05 19:50:04 -0600

    added test module

---
+++ _wilds/test.js
@@ -0,0 +1,17 @@
+/*{
+  "id":"test",
+	"scope":["server"],
+	"desc":"test module",
+}*/
+
+var telepath = require('tele')
+
+module.exports = function () {
+  var self = this
+  telepath(this)
+
+  this.recv = function (data) {
+  }
+
+}
+
