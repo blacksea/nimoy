86540b5b7551f3cfc9a2185769398c20dc509a6e
blacksea <ag@theblacksea.cc>
2013-05-05 21:05:17 -0600

    fixed _wilds/test

--- _wilds/test.js
+++ _wilds/test.js
@@ -9,18 +9,21 @@ var telepath = require('tele')
 module.exports = function () {
   var self = this
   , id = new Date().getTime()
+  console.dir(id)
   telepath(this)
 
-  this.recv = function (data) {
-    console.dir('test: '+data)
+  this.recv = function (json) {
+    var data = JSON.parse(json)
+    console.dir('test recv: '+data)
   }
 
   setInterval(function () {
     var val = Math.random()
     self.send({set:[id,val]})
-  }, 200)
+  }, 400)
+
   setInterval(function () {
     self.send({get:id})
-  }, 75)
+  }, 800)
 }
 
