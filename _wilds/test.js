7b09c2083436c0d0f62ab18363a949eca6fc38c9
blacksea <ag@theblacksea.cc>
2013-05-05 20:06:54 -0600

    updated _wilds/test

--- _wilds/test.js
+++ _wilds/test.js
@@ -8,13 +8,19 @@ var telepath = require('tele')
 
 module.exports = function () {
   var self = this
+  , var id = new Date().getTime()
   telepath(this)
 
   this.recv = function (data) {
     console.dir('test: '+data)
-
-
   }
 
+  setInterval(function () {
+    var val = Math.random()
+    self.send({set:[id,val]})
+  }, 200)
+  setInterval(function () {
+    self.send({get:id})
+  }, 75)
 }
 
