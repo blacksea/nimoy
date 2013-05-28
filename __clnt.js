23e3153066e70338aee85d969430d2124913d0b0
blacksea <ag@theblacksea.cc>
2013-05-28 17:00:53 -0600

    on data.client_id __clnt inits new brico with map,usr, and id

--- __clnt.js
+++ __clnt.js
@@ -4,7 +4,17 @@ var websocket = require('websocket-stream')
 , host = window.location.host.replace('www.','')
 , ws = websocket('ws://'+host)
 
-// need to get key and user
-var brico = new bricoleur()
+var brico = new bricoleur() // need to get key and user
 ws.pipe(brico.in)
 brico.out.pipe(ws)
+
+ws.on('data', function (buffer) {
+  var data = JSON.parse(buffer)
+  if (data.client_id) {
+    var brico = new bricoleur(data.usr)
+    brico.recv(JSON.stringify(data.map))
+    brico.client_id = data.client_id
+    ws.pipe(brico.in) 
+    brico.out.pipe(ws)
+  }
+})
