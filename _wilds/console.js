e164eb7756b3333c160bd5f9318c82e17dca738f
blacksea <ag@theblacksea.cc>
2013-05-29 16:31:33 -0600

    fixed console module form methods

--- _wilds/console.js
+++ _wilds/console.js
@@ -15,43 +15,27 @@ module.exports = function () {
     console.dir(data)
   }
 
-  setInterval(function () {
-    self.send({test:Math.random()})
-  }, 300)
+  this.render = function (html) {
+    var container = document.getElementById('container')
+    , log = document.createElement('div')
+    log.innerHTML = html
+    container.appendChild(log)
+    var form = document.getElementById('x')
+    , prompt = form.querySelector('.console')
 
-  // this.template = null;
-  // this.output = new Stream();
+    form.onsubmit = function (e) {
+      e.preventDefault()
+      var cmd = e.target[0].value
+      self.send({cmd:cmd})
+      prompt.blur()
+    }
 
-  // this.init = function () {
+    prompt.onfocus = function () {
+      prompt.value = ''
+    }
 
-  //   var container = document.getElementById('container')
-  //   , log = document.createElement('div');
-
-  //   log.setAttribute('id','console');
-  //   log.innerHTML = self.template;
-
-  //   container.appendChild(log);
-
-  //   var form = document.getElementById('x')
-  //   , prompt = form.querySelector('.console');
-
-  //   form.onsubmit = function (e) {
-  //     e.preventDefault();
-  //     var cmd = e.target[0].value; // entered text
-  //     self.output.emit('data',cmd);
-  //     prompt.blur();
-  //   }
-  //   
-  //   prompt.onfocus = function () {
-  //     prompt.value = '';
-  //   }
-
-  //   prompt.onblur = function () {
-  //     prompt.value = '>';
-  //   }
-  // }
-
-  // this.output.on('data', function (data) {
-  //   console.dir(data);
-  // });
+    prompt.onblur = function () {
+      prompt.value = '>'
+    }
+  }
 }
