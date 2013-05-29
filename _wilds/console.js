2eb3f46d72037da0fbbab2fe40adb2aa516b7451
blacksea <ag@theblacksea.cc>
2013-05-29 13:34:41 -0600

    using _wilds/console for client testing

--- _wilds/console.js
+++ _wilds/console.js
@@ -4,45 +4,54 @@
 	"desc":"cli",
   "deps":["console.html","console.styl"]
 }*/
-
-var Stream = require('stream');
+var telepath = require('tele')
 
 module.exports = function () {
+  var self = this
+  telepath(this)
 
-  var self = this;
-  this.template = null;
-  this.output = new Stream();
-
-  this.init = function () {
-
-    var container = document.getElementById('container')
-    , log = document.createElement('div');
-
-    log.setAttribute('id','console');
-    log.innerHTML = self.template;
-
-    container.appendChild(log);
-
-    var form = document.getElementById('x')
-    , prompt = form.querySelector('.console');
-
-    form.onsubmit = function (e) {
-      e.preventDefault();
-      var cmd = e.target[0].value; // entered text
-      self.output.emit('data',cmd);
-      prompt.blur();
-    }
-    
-    prompt.onfocus = function () {
-      prompt.value = '';
-    }
-
-    prompt.onblur = function () {
-      prompt.value = '>';
-    }
+  this.recv = function (buffer) {
+    var data = JSON.parse(buffer.toString())
+    console.dir(data)
   }
 
-  this.output.on('data', function (data) {
-    console.dir(data);
-  });
+  setInterval(function () {
+    self.send({test:Math.random()})
+  }, 300)
+
+  // this.template = null;
+  // this.output = new Stream();
+
+  // this.init = function () {
+
+  //   var container = document.getElementById('container')
+  //   , log = document.createElement('div');
+
+  //   log.setAttribute('id','console');
+  //   log.innerHTML = self.template;
+
+  //   container.appendChild(log);
+
+  //   var form = document.getElementById('x')
+  //   , prompt = form.querySelector('.console');
+
+  //   form.onsubmit = function (e) {
+  //     e.preventDefault();
+  //     var cmd = e.target[0].value; // entered text
+  //     self.output.emit('data',cmd);
+  //     prompt.blur();
+  //   }
+  //   
+  //   prompt.onfocus = function () {
+  //     prompt.value = '';
+  //   }
+
+  //   prompt.onblur = function () {
+  //     prompt.value = '>';
+  //   }
+  // }
+
+  // this.output.on('data', function (data) {
+  //   console.dir(data);
+  // });
 }
