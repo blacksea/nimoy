8309037c33110c15a56934b20895701d30097b20
blacksea <ag@theblacksea.cc>
2013-06-07 18:52:25 -0600

    updated _widls/mdisp

--- _wilds/mdisp.js
+++ _wilds/mdisp.js
@@ -8,34 +8,22 @@ var telepath = require('tele')
 
 module.exports = function () {
   var self = this
+  , element = null
   telepath(this)
 
   this.recv = function (buffer) {
     var data = JSON.parse(buffer.toString())
     console.dir(data)
+    if (data.dest==='mdisp') {
+     console.log('write!') 
+    }
   }
 
   this.render = function (html) {
     var container = document.getElementById('container')
-    , log = document.createElement('div')
-    log.innerHTML = html
-    container.appendChild(log)
-    var form = document.getElementById('x')
-    , prompt = form.querySelector('.console')
-
-    form.onsubmit = function (e) {
-      e.preventDefault()
-      var cmd = e.target[0].value
-      self.send({cmd:cmd})
-      prompt.blur()
-    }
-
-    prompt.onfocus = function () {
-      prompt.value = ''
-    }
-
-    prompt.onblur = function () {
-      prompt.value = ''
-    }
+    disp = document.createElement('div')
+    console.log(html)
+    disp.innerHTML = html
+    container.appendChild(disp)
   }
 }
