67ab3b2245aa2fb909078da8a388b25edd0d39e2
blacksea <ag@theblacksea.cc>
2013-06-20 15:05:21 -0600

    updated map

--- _map.js
+++ _map.js
@@ -3,6 +3,7 @@ var telepath = require('tele')
 , uglifyJS = require('uglify-js')
 , stylus = require('stylus')
 , asyncMap = require('slide').asyncMap
+, chain = require('fern').chain
 , fs = require('fs')
 
 module.exports = function (opts) { // MAPPER
@@ -14,7 +15,8 @@ module.exports = function (opts) { // MAPPER
   , destJS = './_wilds/_bundle.js'
   , CSS = ''
 
-  if (opts.watch === true) fs.watch(opts.dir, function (event, file) { // dynamic module reloading
+  // watch _wilds dir and reload brico's on change
+  if (opts.watch === true) fs.watch(opts.dir, function (event, file) { 
     if (event === 'change') {
       fs.stat(opts.dir+'/'+file, function (err, stats) {
         if (err) throw new Error(err)
@@ -27,96 +29,81 @@ module.exports = function (opts) { // MAPPER
     }
   })
  
-  this.survey = function () { 
-    async.waterfall([
-      ReadDir,
-      HandleFiles,
-    ], function (err, result) {
-      compileJS()
-      compileCSS()
+  this.survey = function (cb) { 
+    var map = []
+    , first = chain.first
+    , last = chain.last
+
+    fs.readdir(opts.dir, function (err, files) {
       if (err) throw new Error(err)
-    })
-
-    function ReadDir (cb) {
-      fs.readDir(opts.dir, function (err, files) {
-        if (err) throw new Error(err)
-        cb(null, files)
-      })
-    }
-
-    function HandleFiles (err, cb) {
-      async.each(files, function (file, cb) {
-        var filepath = opts.dir+'/'+file
-        if (file.split('.')[1] === 'js') fs.readFile(filepath, function (err, buffer) {
-          if (err) throw err
-          var data = buffer.toString()
-          , moduleData = null
-          , buf = ''
-
-          for (var i=0;i<data.length;i++) { // parse out data object
-            buf += data[i]
-            if (data[i] === '}' && data[1] === '*' && data[2] === '{') {
-              moduleData = JSON.parse(buf.toString().replace('/*',''))
-              moduleData.filePath = filePath
-              break
-            }
-          }
-
-          if (moduleData && moduleData.deps) { // if there are deps handle them
-            async.each(moduleData.deps, function (dep, cb) {
-              fs.readFile(dir+'/'+dep, function (err,buffer) {
-                if (err) throw err
-                moduleData[dep.split('.')[1]] = buffer.toString()
-                cb()
-              })
-            }, function () {
-              self.send(moduleData)
-              cb()
-            })
-          } else if (moduleData) {
-            self.send(moduleData)
-            cb()
-          } else cb()
-        })
-        else cb()
-      }, cb)
-    }
-  }
-
-  function compileCSS (cb) {
-    fs.readFile(opts.css, function (err, buffer) { // handle css
-      if (err) throw err
-      var styles = buffer.toString()
-      styles += CSS
-      stylus.render(styles, {filename:destCSS}, function (err, css) {
-        if (err) throw err
-        fs.writeFile(destCSS, css, function (err) {
-          if (err) throw err
-          cb(null)
-        })
+      asyncMap(files, HandleFile, function () {
+        // send map
+        compile(cb) 
       })
     })
   }
-  
-  function compileJS (cb) { 
-    var b = browserify(opts.js)
 
-    async.forEach(opts.js, function (item, cb) {
-      var path = item.split('/')
-      if (path[1] === '_wilds') b.require(item, {expose:path[2].replace('.js','')}) 
-      cb()
-    }, function () {
-      b.bundle(function (err, bundle) {
-        if (err) throw err
-        if (opts.compress === true) {
-          var bundleMin = uglifyJS.minify(bundle,{fromString: true})
-          bundle = bundleMin.code
+  function HandleFile (file, cb) {
+    var filepath = opts.dir+'/'+file
+    if (file.split('.')[1] === 'js') fs.readFile(filepath, function (err, buffer) {
+      var data = buffer.toString()
+      , moduleData = null
+      , buf = ''
+
+      for (var i=0;i<data.length;i++) { // parse out data object
+        buf += data[i]
+        if (data[i] === '}' && data[1] === '*' && data[2] === '{') {
+          moduleData = JSON.parse(buf.toString().replace('/*',''))
+          moduleData.filePath = filePath
+          break
         }
-        fs.writeFile(destJS, bundle, function (err) {
-          if (err) throw err
-          cb(null)
+      }
+
+      if (moduleData && moduleData.deps) { // if there are deps handle them
+        async.each(moduleData.deps, function (dep, cb) {
+          fs.readFile(dir+'/'+dep, function (err,buffer) {
+            moduleData[dep.split('.')[1]] = buffer.toString()
+            cb()
+          })
+        }, function () {
+          self.send(moduleData)
+          cb()
         })
-      })   
+      } else if (moduleData) {
+        self.send(moduleData)
+        cb()
+      } else cb()
     })
+    else cb()
+  }
+
+  function compile (cb) {
+    fs.readFile(opts.css, function (err, buffer) { // handle css
+      if (err) cb(err)
+      var styles = buffer.toString()
+      stylus.render(styles, {filename:destCSS}, function (err, css) {
+        if (err) cb(err)
+        fs.writeFile(destCSS, css, compilejs) 
+      })
+    })
+
+    function compilejs () { 
+      var b = browserify(opts.js)
+      asyncMap(opts.js, function (item, cb) {
+        var path = item.split('/')
+        if (path[1] === '_wilds') b.require(item, {expose:path[2].replace('.js','')}) 
+        cb()
+      }, function () {
+        b.bundle(function (err, bundle) {
+          if (opts.compress === true) {
+            var bundlemin = uglifyjs.minify(bundle,{fromstring: true})
+            bundle = bundlemin.code
+          }
+          fs.writeFile(destJS, bundle, function (err) {
+            cb(err)
+          })
+        })   
+      })
+    }
   }
 }
