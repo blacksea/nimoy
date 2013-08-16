97d2e239c717a0e206f342ab9ba99975be604a33
blacksea <ag@theblacksea.cc>
2013-08-16 16:09:50 -0600

    adding _data to interface with a leveldb for object storage

---
+++ _data.js
@@ -0,0 +1,16 @@
+var level = require('level')
+, Dupelx = require('stream').Duplex
+, util = require('util')
+
+util.inherits(Data, Duplex)
+
+module.exports = Data
+
+function Data (opts) {
+  if (!(this instanceof Data)) return new Data(opts)
+  Duplex.call(this)
+
+  var SELF = this
+  
+
+}
