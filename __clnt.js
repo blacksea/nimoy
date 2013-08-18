13b51deb6419990b1279789d1f569263ac3e43fb
blacksea <ag@theblacksea.cc>
2013-08-18 14:23:45 -0600

    rewrite init/brico create

--- __clnt.js
+++ __clnt.js
@@ -1,17 +1,22 @@
 // CLIENT START SCRIPT
 var websocket = require('websocket-stream')
-, bricoleur = require('./_brico')
+, bricoleur = require('./_brico.js')
 , host = window.location.host.replace('www.','')
 , ws = websocket('ws://'+host)
 
-ws.on('data', function (buffer) {
-  var data = JSON.parse(buffer)
-  if (data.client_id) {
-    var brico = new bricoleur(data.usr)
-    brico.map.client = data.map
-    brico.client_id = data.client_id
-    ws.pipe(brico.in) 
-    brico.out.pipe(ws)
-    brico.build()
-  }
-})
+var brico = new bricoleur()
+
+ws.pipe(brico)
+
+// ws.on('data', function (buffer) {
+//   console.log(buffer)
+//   var data = JSON.parse(buffer)
+//   if (data.client_id) {
+//     var brico = new bricoleur(data.usr)
+//     brico.map.client = data.map
+//     brico.client_id = data.client_id
+//     ws.pipe(brico.in) 
+//     brico.out.pipe(ws)
+//     brico.build()
+//   }
+// })
