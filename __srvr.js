fb6db0d726f12fa0ad9ed421198ae9258b1a4467
blacksea <ag@theblacksea.cc>
2013-08-19 00:11:43 -0600

    debugging soc conn

--- __srvr.js
+++ __srvr.js
@@ -7,26 +7,28 @@ var Bricoleur = require('./_brico')
 , http = require('http')
 , fs = require('fs')
 , ws = require('ws').Server
+, wsstream = require('websocket-stream')
 
 var port = 80 // set port
 
 var brico = new Bricoleur()
 var _cmp = new Compiler({end:false,compress:false})
 
-// brico.pipe(process.stdout)
-
 var _map = new map({dir:'./_wilds'}, function mapStream (s) {
   s.server.pipe(brico)
   s.client.pipe(_cmp)
 })
-
-// _map.on('change', function (stat) {
-//   console.log(stat)
-// })
 
 var _rtr = new rtr() // do routing 
 var server = http.createServer(_rtr.handleReqs) // handle http requests
 server.listen(port)
 
 var wss = new ws({server:server})
-wss.on('connection', _rtr.handleSoc)
+// wss.on('connection', _rtr.handleSoc)
+wss.on('connection', function handleSoc (soc) {
+  var s = wsstream(soc)
+  console.log(soc)
+  _cmp.getMods(function (mods) {
+    s.write(JSON.stringify(mods[0]))
+  })
+})
