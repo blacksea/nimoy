26e596cff8c307573fe57d372ffaa79baeb1854c
blacksea <ag@theblacksea.cc>
2013-08-21 14:41:23 -0600

    stripped down

--- __clnt.js
+++ __clnt.js
@@ -1,17 +1,9 @@
 // CLIENT START SCRIPT
-var websocket = require('websocket-stream')
-, bricoleur = require('./_brico.js')
+var Bricoleur = require('./_brico.js')
+, websocket = require('websocket-stream')
 , host = window.location.host.replace('www.','')
 , ws = websocket('ws://'+host)
 
-var brico = new bricoleur()
+var brico = new Bricoleur()
 
-setTimeout(function () {
-  brico.conn(['console+mdisp'])
-}, 3000)
-
-ws.on('data', function (buffer) {
-  var data = JSON.parse(buffer)
-  console.log(data)
-  if (data.id) brico.make(data)
-})
+ws.pipe(brico).pipe(ws)
