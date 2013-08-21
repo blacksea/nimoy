5adcb511d69c3a67b2fb0e0ad7c9feffc8d21c0d
blacksea <ag@theblacksea.cc>
2013-08-21 16:47:52 -0600

    implementing layered env

--- _brico.js
+++ _brico.js
@@ -13,31 +13,48 @@ function Bricoleur (opts) {
   , MAP = null
   , ENV = {}
   , _ = {} // module scope
+  
+  this.addSoc = function (key, cb) { // user socket connection
+    self[key] = new stream.Duplex
+    self[key]._read = function (size) {}
+    self[key]._write = function (chunk,enc,next) { //add key to each obj
+      var d = JSON.parse(chunk.toString())
+      d.k = key
+      self.recv(d)
+      next()
+    }
+    var s = self[key]
+    s.id = key
+    cb()
+  }
 
-  this._read = function (size) {} // !?!
-  this.end = function () {}
+  this.rmSoc = function (key) {
+    self[key].emit('close')
+    delete self[key]
+  } 
 
-  this.write  = function (chunk) {
-    var d = JSON.parse(chunk.toString())
+  this.send = function (d) {
+    if (!d.k) console.error('no client id')
+    if (d.k) self[d.k].emit('data', JSON.stringify(d))
+  }
+  this.recv = function (d) {
     if (d.r&&d.v) handleRoute(d)
     if (d.cmd&&d.val) self[d.cmd](d.val) // point cmd to internal function 
   }
 
   function handleRoute (d) {
-    switch (d.r) {
-      case 'key' : self.ID = d.v; console.log(self.ID);break;
-      default : console.error('route not recognized');break;
-    } 
+      switch (d.r) {
+        case 'key' : self.ID = d.v; console.log(self.ID);break;
+        default : console.error('route not recognized');break;
+      } 
   }
-
-  this.make = function (mod) {
+  function make (mod) {
     if (process.browser&&mod.html){
       var m = require(mod.id.toUpperCase())
       _[mod.id] = new m(mod.html)
     }
   }
-
-  this.conn = function (conns) {
+  function conn (conns) {
     conns.forEach(function handleConnection (conn) {
       if (conn.match(/\+/)!==null) {
         var modA = conn.split('+')[0]
@@ -52,26 +69,16 @@ function Bricoleur (opts) {
     })
   }
 
-  this.unmake = function (mod) {
+  function unmake (mod) {
     if (process.browser) mod.destroy()
   }
 
-  this.addConnection = function (key, cb) { // user socket connection
-    self[key] = new stream.Duplex
-    self[key]._read = function (size) {}
-    self[key]._write = function (chunk,enc,next) {
-      self.write(chunk)
-      next()
-    }
-    var s = self[key]
-    s.id = key
-    cb()
+  // utility env stream
+  this._read = function (size) {} // !?!
+  this.end = function () {}
+  this.write  = function (chunk) { // get map stream
+    var d = JSON.parse(chunk.toString())
   }
-
-  this.removeConnection = function (key) {
-    self[key].emit('close')
-    delete self[key]
-  } 
 }
 
 inherits(Bricoleur, stream.Stream)
