061eec51d2c8b705444d138ba9cb5558bb7f228f
blacksea <ag@theblacksea.cc>
2013-08-25 15:08:17 -0600

    remove instanceof

--- _brico.js
+++ _brico.js
@@ -2,8 +2,7 @@ var stream = require('stream')
 , Duplex = require('stream').Duplex
 , inherits = require('inherits')
 
-function Bricoleur (opts) { 
-  if (!(this instanceof Bricoleur)) return new Bricoleur(opts)
+function Bricoleur (opts) { // BRICOLEUR
   stream.Stream.call(this)
   if (!opts) opts = {}
   this.readable = true
@@ -14,55 +13,62 @@ function Bricoleur (opts) {
   , MAP = null
   , ENV = {}
   , _ = {} // module scope
+
+  // _.com = new Duplex // module layer
   
-  this.addSoc = function (key, cb) { // user socket connection
+  this.socAdd = function (key, cb) { // user socket connection
     self[key] = new stream.Stream
     self[key].writable = true
     self[key].readable = true
-    self[key].read = function (size) {}
+    self[key].write = function (chunk) {
+      console.log(chunk)
+    }
+    self[key]._read = function (size) {}
+    self[key].on('error', function (e) {
+      console.error(e)
+    })
     self[key].end = function () {
       console.log('closed')
     }
-    self[key].write = function (chunk) { //add key to each obj
-      var d = JSON.parse(chunk.toString())
-      d.k = key
-      self.recv(d)
-    }
+    //self[key].pipe(_.com).pipe(self[key]) // patch into module scope
     cb()
   }
-  this.rmSoc = function (key) {
-    self[key].emit('close')
-    delete self[key]
-  } 
-
-  this.send = function (d) {
+  this.socSend = function (d) {
     if (!d.k) console.error('no client id')
     if (d.k) self[d.k].emit('data', JSON.stringify(d))
   }
-  this.recv = function (d) {
+  this.socRecv = function (d) {
     if (d.r&&d.v) handleRoute(d)
   }
-
+  this.socRm = function (key) {
+    self[key].emit('close')
+    delete self[key]
+  } 
+  
   function handleRoute (d) {
+
     switch (d.r) {
-      case 'key' : self.ID = d.v; console.log(self.ID);break;
-      default : console.error('route not recognized');break;
+      case 'key' : self.ID = d.v; console.log(self.ID); break;
+      default : console.error('route not recognized'); break;
     } 
   }
   function make (mod) {
-    if (process.browser&&mod.html){
+    if (process.browser&&mod.html) {
       var m = require(mod.id.toUpperCase())
       _[mod.id] = new m(mod.html)
     }
   }
+  function unmake (mod) {
+    if (process.browser) mod.destroy()
+  }
   function conn (conns) {
     conns.forEach(function handleConnection (conn) {
-      if (conn.match(/\+/)!==null) {
+      if (conn.match(/\+/) !== null) {
         var modA = conn.split('+')[0]
         , modB = conn.split('+')[1]
         _[modA].pipe(_[modB])
       }
-      if (conn.match(/\-/)!==null) {
+      if (conn.match(/\-/) !== null) {
         var modA = conn.split('-')[0]
         , modB = conn.split('-')[1]
         modA.unpipe(modB)
@@ -70,11 +76,6 @@ function Bricoleur (opts) {
     })
   }
 
-  function unmake (mod) {
-    if (process.browser) mod.destroy()
-  }
-
-  // utility env stream
   this._read = function (size) {} // !?!
   this.end = function () {}
   this.write  = function (chunk) { // get map stream
