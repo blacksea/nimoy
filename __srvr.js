75f2563cbdb354ff268d1a5e0741fff33d016c7b
blacksea <ag@theblacksea.cc>
2013-08-27 17:44:40 -0600

    update for changes to env&map

--- __srvr.js
+++ __srvr.js
@@ -2,7 +2,7 @@
 var Bricoleur = require('./_brico')
 , data = require('./_data')
 , map = require('./_map')
-, rtr = require('./_env')
+, env = require('./_env')
 , http = require('http')
 , fs = require('fs')
 , ws = require('ws').Server
@@ -12,12 +12,13 @@ var port = 80 // set port
 
 var brico = new Bricoleur()
 
+var _env = new env()
+
 var _map = new map({dir:'./_wilds'}, function mapStream (s) {
-  s.pipe(env)
+  s.pipe(_env)
 })
 
-var _rtr = new rtr() // do routing 
-var server = http.createServer(_rtr.handleReqs) // handle http requests
+var server = http.createServer(_env.handleReqs) // handle http requests
 server.listen(port)
 
 var wss = new ws({server:server})
