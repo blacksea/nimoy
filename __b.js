342c9af44d5751b39a064511759158ff4fb3086e
blacksea <ag@theblacksea.cc>
2013-08-28 00:34:26 -0600

    setting up browser process

--- __b.js
+++ __b.js
@@ -1,16 +1,8 @@
-// CLIENT START SCRIPT
-var Bricoleur = require('./_brico.js')
-, websocket = require('websocket-stream')
-, host = window.location.host.replace('www.','')
-, ws = websocket('ws://'+host)
+// BROWSER ENVIRONMENT 
+var Env = require('./_env.js').browser
 
-var brico = new Bricoleur()
-
-ws.on('data', function (buf) {
-  var d = JSON.parse(buf.toString())
-  if (d.k) {
-    brico.addSoc(d.k, function connectionAdded () {
-      ws.pipe(brico[d.k]).pipe(ws)
-    })
-  }
+var _env = new Env({
+  
+}, function () {
+  console.log('created a new environment')
 })
