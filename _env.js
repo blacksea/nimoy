a3b0502968b9afc7542ad0134858cfe2753f0329
blacksea <ag@theblacksea.cc>
2013-09-03 17:39:55 -0600

    change process classnames

--- _env.js
+++ _env.js
@@ -1,25 +1,26 @@
 // ENVIRONMENT
-var filed = require('filed')
-, Bricoleur = require('./_brico')
-, Compiler = require('./_cmp')
-, Map = require('./_map')
-, Data = require('./_data')
-, Duplex = require('stream').Duplex
+var Bricoleur = require('./_brico')
 , inherits = require('inherits')
-, ws = require('ws').Server
-, http = require('http')
-, asyncMap = require('slide').asyncMap
-, websocketStream = require('websocket-stream')
 
-inherits(Env, Duplex)
-module.exports = Env
+module.exports.nodeEnv = nodeEnv
+module.exports.browserEnv = browserEnv
 
-function Env (opts) { 
-  var browserMods = []
-  , self = this
-  Duplex.call(this)
+function browserEnv (opts, loaded) { // BROWSER ENVIRONMENT
+  // no mapping functionailty is available
+  loaded()
+}
 
-  this._read = function (size) {}
+function nodeEnv (opts, loaded) { // NODE ENVIRONMENT
+  var filed = require('filed')
+  var Map = require('./_map')
+  var Compiler = require('./_cmp')
+  var Data = require('./_data')
+  var ws = require('ws').Server
+  var http = require('http')
+  var asyncMap = require('slide').asyncMap
+  var websocketStream = require('websocket-stream')
+
+  var self = this
 
   if (!opts) var opts = [ 
     {url:"/",
@@ -34,13 +35,27 @@ function Env (opts) {
     s.pipe(self)
   })
 
+  var _cmp = new Compiler({
+    compress:false,
+    stylesPath:'./_wilds/_css.styl',
+    jsPath:'./__clnt.js',
+    cssPath: './_wilds/_styles.css',
+    bundlePath:'./_wilds/_bundle.js'
+  })
+
+  _cmp.on('data', function (d) {
+
+  })
+
   var brico = new Bricoleur()
 
   var Server = http.createServer(handleReqs)
   Server.listen(opts.port)
-
+  
   var soc = new ws({server:Server})
-  soc.on('connection', function handleSoc (s) {
+  soc.on('connection', newSocket)
+
+  function newSocket (soc) {
     var wss = websocketStream(s)
     , headers = soc.upgradeReq.headers
     , key = headers['sec-websocket-key']
@@ -59,30 +74,6 @@ function Env (opts) {
       }
       s.write(JSON.stringify(cmd))
     }, 200)
-  })
-
-  var _cmp = new Compiler({
-    compress:false,
-    stylesPath:'./_wilds/_css.styl',
-    jsPath:'./__clnt.js',
-    cssPath: './_wilds/_styles.css',
-    bundlePath:'./_wilds/_bundle.js'
-  })
-
-  self.pipe(_cmp)
-
-  this.write = function (chunk) {
-    var mod = JSON.parse(chunk.toString())
-    mod.process.forEach(function (pro) {
-      if (pro==='browser') {
-        self.push(chunk)
-        browserMods.push(mod)
-      }
-    })
-  }
-
-  this.end = function () {
-    console.log('compile!')
   }
 
   this.handleReqs = function (req, res) {
