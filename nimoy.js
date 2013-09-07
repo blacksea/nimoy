92715d0341f6e2665532e54c100d740f3ae8fb0d
blacksea <ag@theblacksea.cc>
2013-09-07 02:33:28 -0600

    add default user to env

--- nimoy.js
+++ nimoy.js
@@ -1,9 +1,18 @@
-// SERVER START SCRIPT
+// NIMOY 
 var Env = require('./_env_N')
 var _env = new Env({
   port:80,
   wilds: './_wilds'
 })
+
+var defaultUser = {
+  name : 'default',
+  host : 'localhost',
+  connections: ''
+  modules : '',
+}
+_env.addUser(defaultUser)
+
 _env.load(function environmentLoaded () {
   console.log('nimoy active!')
 })
