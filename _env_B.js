da69ce6df7be62cb123cc81e69a3a7610c439e55
blacksea <ag@theblacksea.cc>
2013-09-09 19:50:24 -0600

    added brico to env_B

--- _env_B.js
+++ _env_B.js
@@ -1,15 +1,17 @@
 // BROWSER ENVIRONMENT 
 if(!Function.prototype.bind) require('bindshim') //kindle jalopy doesn't have bind
 var host = window.document.location.host.replace(/:.*/, '')
+var Bricoleur = require('./_brico')
 var ws = require('websocket-stream')
 var wss = ws('ws://'+host)
 
+var b = new Bricoleur()
+
+wss.pipe(b.metaStream).pipe(wss)
+
 wss.on('open', function () {
-  window.document.title = 'connnnnnn'
+  console.log('socket open')
 })
-wss.on('data', function (d) {
-  console.log(d)
-})
-wss.on('end', function () {
-  console.log('server close')
+wss.on('close', function () {
+  console.log('server connection closed')
 })
