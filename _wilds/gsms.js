7f56b13f99eb29852eda07b82f74673eaab92d8c
blacksea <ag@theblacksea.cc>
2013-09-10 16:45:19 -0600

    remove render from gsms for now

--- _wilds/gsms.js
+++ _wilds/gsms.js
@@ -14,34 +14,10 @@ function Gsms (template) {
   this.s = through(write,end,{autoDestroy:false})
 
   function write (chunk) {
+    console.log(chunk)
   }
+
   function end () {
-  }
-
-  render(template)
-
-  function render (html) {
-    console.log(html)
-    var container = document.getElementById('container')
-    , log = document.createElement('div')
-    log.innerHTML = html
-    container.appendChild(log)
-    var form = document.getElementById('x')
-    , prompt = form.querySelector('.console')
-
-    form.onsubmit = function (e) {
-      e.preventDefault()
-      var cmd = e.target[0].value
-      self.emit('data', JSON.stringify({cmd:cmd}))
-      prompt.blur()
-    }
-
-    prompt.onfocus = function () {
-      prompt.value = ''
-    }
-
-    prompt.onblur = function () {
-      prompt.value = ''
-    }
+    console.log('stream end')
   }
 }
