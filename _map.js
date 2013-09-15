e424fa7586778bbeca10a0278659232c28716e13
blacksea <ag@theblacksea.cc>
2013-09-14 21:15:10 -0600

    cleanedgit --git-dir=/home/agasca/src/nimoy/.git push ghub master

--- _map.js
+++ _map.js
@@ -4,52 +4,46 @@ var asyncMap = require('slide').asyncMap
 var fs = require('fs')
 
 inherits(Map, Readable)
+
 module.exports = Map
 
 function Map (opts, callback) {
   Readable.call(this)
 
-  var L = opts.path_wilds[opts.path_wilds.length-1]
-  if (L !== '/') opts.path_wilds += '/'
+  if (opts.path_wilds[opts.path_wilds.length-1] !== '/') opts.path_wilds += '/'
 
-  var self = this
-  var FILESTAT 
+  var Self = this
+  var FileStat 
 
   this._read = function (size) {} // WTF!
 
-  fs.readdir(opts.path_wilds, function handleWildsFiles (e, files) {
-    callback(self)
-    asyncMap(files, parse, function doneWildsFiles () {
-      self.emit('end')
-    })
-  })
-
-  // WATCH WILDS DIR FOR MODULE UPDATES AND RELOAD
   fs.watch(opts.path_wilds, function handleFileChange (event, file) { 
     var filepath = opts.path_wilds+file
     fs.stat(filepath, function statFile (err, stats) {
-      // make a good data object here
+      if (e) console.error(e)
       stats.filepath = filepath
-      if (err) console.error(err)
-      if (!FILESTAT) FILESTAT = stats
-      if (FILESTAT.size !== stats.size) {
+      if (!FileStat) FileStat = stats
+      if (FileStat.size !== stats.size) {
         var mod = stats.filepath.split('/')[2]
         var ext = mod.split('.')[1]
         var file = mod
         if (ext !== 'js') file = mod.split('.')[0]+'.js' 
-        if (file[0] !== '_') {
-          parse(file, function () {
-            console.log('updated '+file)
-          })
-        }
+        Parse(file, function () {
+          console.log('updated '+file)
+        })
       } 
-      FILESTAT = stats
+      FileStat = stats
     })
   })
 
-  // modify module data to test process /rpi?/amd64 etc...
-  
-  function parse (file,cb) {
+  fs.readdir(opts.path_wilds, function handleWildsFiles (e, files) {
+    callback(self)
+    asyncMap(files, Parse, function doneWildsFiles () {
+      self.emit('end')
+    })
+  })
+ 
+  function Parse (file,cb) {
     var ext = file.split('.')[1]
     if (ext === 'js' && file[0] !=='_') {
       var f = fs.createReadStream(opts.path_wilds+file)
