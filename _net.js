83f75ec7479010eb6d981dcabf07e0b7f82d96d6
nimoy <ag@theblacksea.cc>
2013-11-13 18:37:20 -0700

    changed structure, rm proc, add _net & _dat

--- _net.js
+++ _net.js
@@ -1,3 +1,28 @@
+// BROWSER ENVIRONMENT 
+
+if(!Function.prototype.bind) require('bindshim') //kindle jalopy doesn't have bind
+var host = window.document.location.host.replace(/:.*/, '')
+var ws = require('websocket-stream')
+var through = require('through')
+var wss = ws('ws://'+host)
+
+var Bricoleur = require('./_brico')
+
+var b = new Bricoleur()
+
+b.addSocket(host, function () {
+  wss.pipe(b[host]).pipe(wss)
+})
+
+wss.on('open', function () {
+  console.log('socket open')
+  wss.write(JSON.stringify({api:{cmd:'test',msg:'xorb'}}))
+})
+
+wss.on('close', function () {
+  console.log('server connection closed')
+})
+
 // NODE ENVIRONMENT
 
 // BASE SYSTEM WITH PLUGIN SOCKETS
