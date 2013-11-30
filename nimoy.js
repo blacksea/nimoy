0c5d8e776368be503438ce2257c61ea5f0ddedbb
nimoy <ag@theblacksea.cc>
2013-11-30 16:11:51 -0700

    update nimoy

--- nimoy.js
+++ nimoy.js
@@ -1,10 +1,9 @@
 // NIMOY 
 
-// -----------------------------------------------|
-// trigger. build the system with specific options
-// -----------------------------------------------|
-
-var Environment = require('./_env_N')
+var Brico = require('./_brico')
+var Data = require('./_data')
+var Map = require('./_map')
+var Net = require('./_net')
 
 var DefaultUser = {
   host:'blacksea',
@@ -23,110 +22,3 @@ var EnvOpts = {
   port:80,
 }
 
-// | ------------------------------------------------------------------
-// | match process to scope in mod/desc ex. browser, pi, joyent, debian
-// | provide process capabilities
-// | -------------------------------------------------
-// | implement level namespacing w. bookmarked modules
-// | ----------------
-// | port in watchify
-// | ----------------
-// | build ui in paperjs
-// | ----------------------
-// | implement modes & auth
-// | -----------------------
-// | build in module storage
-// | -----------------------------------------------------
-// | socket switch / router -- webRTC datachannel support?
-// | -----------------------------------------------------
-
-var Env = new Environment(EnvOpts, function serverRunning () {
-  console.log('nimoy running on port '+EnvOpts.port)
-  Env.api.write(['load',null])
-  Env.api.write(['createBrico',DefaultUser])
-})
-
-// COMPILER
-
-var through = require('through')
-var asyncMap = require('slide').asyncMap
-var uglifyJS = require('uglify-js')
-var stylus = require('stylus') 
-var fs = require('fs')
-
-// USE WATCHIFY TO UPDATE JS 
-// build with paperjs & ignore styles + html
-
-var browserify = require('browserify')
-
-module.exports = Compiler
-
-function Compiler (opts) { 
-  if (opts.path_wilds[opts.path_wilds.length-1] !== '/') opts.path_wilds += '/'
-  var Self = this
-  var Wilds = opts.path_wilds
-  var CSS = ''
-
-  var b = browserify()
-  b.add(opts.path_env)
-
-  fs.readFile(opts.path_styl, function getDefaultStyl (e, buf) {
-    if (e) console.error(e)
-    if (!e) CSS += buf.toString()
-  })
-
-  this.s = through(HandleModule, Compile, {autoDestroy:false})
-
-  function HandleModule (chunk) {
-    var s = this
-    var mod = JSON.parse(chunk.toString())
-
-    b.require(Wilds+mod.id+'.js',{expose:mod.id}) 
-
-    // handle deps
-    if (!mod.deps) s.queue(chunk.toString()) 
-    if (mod.deps) asyncMap(mod.deps, handleDep, function () {
-      s.queue(JSON.stringify(mod))
-      if (mod.fresh===true) Compile()
-    })
-    function handleDep (file, next) {
-      var dep = ''
-      var ext = file.split('.')[1]
-      fs.readFile(Wilds+file, function (e,buf) {
-        if (e) console.error(e)
-        dep = buf.toString()
-        if (ext==='styl') CSS += dep
-        if (ext==='html') mod[ext] = dep
-        next()
-      })
-    }
-  }
-
-  function Compile () {
-    Self.s.emit('data', JSON.stringify({stat:'mapReady'}))
-
-    var bundleFile = fs.createWriteStream(opts.path_bundle)
-    b.bundle().pipe(bundleFile)
-
-    bundleFile.on('error', function (e) {
-      console.error(e)
-    })
-    bundleFile.on('close', function () {
-      console.log('wrote '+opts.path_bundle)
-      if (opts.compress === true) {
-        var min = uglifyJS.minify(opts.path_bundle) 
-        fs.writeFile(opts.path_bundle, min.code, function (e) {
-          if (!e) console.log('wrote minified '+opts.path_bundle)
-        })
-      }
-    })
-
-    stylus.render(CSS, {filename:opts.path_css}, function (e, compiledCSS) {
-      if (e) console.error(e)
-      fs.writeFile(opts.path_css, compiledCSS, function (e) {
-        if (e) cosole.error(e)
-        console.log('wrote '+opts.path_css)
-      })
-    })
-  }
-}
