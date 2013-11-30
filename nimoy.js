290a004b2c32a07e1435cf690d963b864563bd12
nimoy <ag@theblacksea.cc>
2013-11-30 16:41:00 -0700

    rough in net

--- nimoy.js
+++ nimoy.js
@@ -11,6 +11,8 @@ var DefaultUser = {
   modules:''
 }
 
+// use config option to set js output location + use with http
+
 var EnvOpts = {
   path_wilds:'./_wilds',
   path_static:'./_static',
@@ -20,5 +22,31 @@ var EnvOpts = {
   path_js:'./_env_B.js',
   path_data:'./data',
   port:80,
+
 }
 
+Server.listen(opts.port, function () {
+  var uid = parseInt(process.env.SUDO_UID) 
+  if (uid) process.setuid(uid) // switch to user permissions
+  // load level now so it doesn't run as sudo
+})
+
+var API = {
+  load: function (u, cb) {
+    var streamBricos = Data.createValueStream()
+    streamBricos.on('data', function (d) {
+      var brico = JSON.parse(d)
+      _[brico.host] = new Bricoleur()
+      _[brico.host].data = level(opts.path_data+brico.host)
+    })
+    streamBricos.on('end', function () {
+      cb('done!')
+    })
+  },
+  createBrico: function (brico, next) {
+    Data.put(brico.key, JSON.stringify(brico), function () {
+      next(brico)
+    }) 
+  }
+}
+this.api = new fern({tree:API})
