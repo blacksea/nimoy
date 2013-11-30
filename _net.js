0a83a4dd8b675064cfcbbdcbe925c58d37027df7
nimoy <ag@theblacksea.cc>
2013-11-30 16:40:48 -0700

    roughed in net

--- _net.js
+++ _net.js
@@ -1,88 +1,68 @@
-// BROWSER ENVIRONMENT 
+// BROWSER NET 
 
-if(!Function.prototype.bind) require('bindshim') //kindle jalopy doesn't have bind
-var host = window.document.location.host.replace(/:.*/, '')
-var ws = require('websocket-stream')
+//kindle jalopy doesn't have bind
+if(!Function.prototype.bind) require('bindshim') 
+  var host = window.document.location.host.replace(/:.*/, '');
+
 var through = require('through')
-var wss = ws('ws://'+host)
-
-var Bricoleur = require('./_brico')
-
-var b = new Bricoleur()
-
-b.addSocket(host, function () {
-  wss.pipe(b[host]).pipe(wss)
-})
-
-wss.on('open', function () {
-  console.log('socket open')
-  wss.write(JSON.stringify({api:{cmd:'test',msg:'xorb'}}))
-})
-
-wss.on('close', function () {
-  console.log('server connection closed')
-})
-
-// NODE ENVIRONMENT
-
-// BASE SYSTEM WITH PLUGIN SOCKETS
 
 var websocketStream = require('websocket-stream')
 var ws = require('ws').Server
 
 var fs = require('fs')
 var http = require('http')
-var filed = require('filed')
 var fern = require('fern')
 
-var map = require('./_map')
-var cmp = require('./_cmp')
-var level = require('level')
+// CONFIGURATION 
 
-var Bricoleur = require('./_brico')
+var CompileOpts = {
+  path_wilds:opts.path_wilds,
+  path_styl:opts.path_styl,
+  path_css:opts.path_css,
+  path_bundle:opts.path_bundle,
+  path_env:opts.path_js,
+  compress:false
+}
 
-module.exports = Environment
+var MapOpts = {
+  path_wilds:opts.path_wilds
+}
 
-function Environment (opts, running) { 
+// might need muxdemux too
+
+// put client side ws connection in here somehow
+// tell browserify to ignore the rest
+
+module.exports.http = netHTTP
+
+module.exports.ws = netWS
+
+// SERVER NET
+
+function netWS (server,soc) {
+  var WebSocket = new ws({server:Server})
+
+  WebSocket.on('connection', HandleSoc)
+
+  function HandleSoc (soc) { // fix this thing!
+    var wss = websocketStream(soc)
+    var headers = soc.upgradeReq.headers
+    var key = headers['sec-websocket-key']
+    var host = headers.host
+    if (headers['sec-websocket-key1']) key = headers['sec-websocket-key1'].replace(/\s/g,'-')
+    // pass out duplex stream object
+  }
+}
+
+function netHTTP (opts,running) {
+
   if (opts.path_data[(opts.path_data.length-1)] !== '/') opts.path_data += '/'
   if (opts.path_static[(opts.path_static.length-1)] !== '/') opts.path_static += '/'
   if (opts.path_wilds[(opts.path_wilds.length-1)] !== '/') opts.path_wilds += '/'
 
   var self = this
   var StaticFiles = {}
-  var MAP = []
-  var Data
 
-  var _ = {} // brico scope container // replace with com core --
-
-  // CONFIGURATION 
-  
-  var CompileOpts = {
-    path_wilds:opts.path_wilds,
-    path_styl:opts.path_styl,
-    path_css:opts.path_css,
-    path_bundle:opts.path_bundle,
-    path_env:opts.path_js,
-    compress:false
-  }
-  var MapOpts = {
-    path_wilds:opts.path_wilds
-  }
-  var Map = new map(MapOpts, function (s) {// map wilds
-    var compiler = new cmp(CompileOpts) 
-    s.pipe(compiler.s)
-
-    compiler.s.on('data', function (chunk) {
-      var d = JSON.parse(chunk)      
-      MAP.push(chunk)
-    })
-
-    s.on('end', function () {
-      console.log('map of '+opts.path_wilds+' complete!')
-    })
-  })   
- 
-  // HTTP SERVER FOR STATIC FILES
   
   fs.readdir(opts.path_static, function GetStaticFiles (e, files) {
     if (e) console.error(e)
@@ -97,69 +77,7 @@ function Environment (opts, running) {
     var url = req.url
     if (url === '/') url = '/index.html'
     var file = url.replace('/','') 
-    if (StaticFiles[file]) filed(StaticFiles[file]).pipe(res)
-    if (!StaticFiles[file]) res.end() // todo: somekindof 404
+    if (StaticFiles[file]) fs.createReadStream(StaticFiles[file])).pipe(res)
+    if (!StaticFiles[file]) res.end('nobody') // todo: somekindof 404
   }
-
-  Server.listen(opts.port, function () {
-    var uid = parseInt(process.env.SUDO_UID) 
-    if (uid) process.setuid(uid) // switch to user permissions
-    // wait until user permissions are active
-    fs.readdir('./data', function (e,d) {
-      if (e && e.errno === 34) { 
-        fs.mkdir('./data', function () {
-          Data = level(opts.path_data+'env') 
-          running()
-        })
-      }
-      if (!e) {
-        Data = level(opts.path_data+'env') 
-        running()
-      }
-    })
-  })
-
-  // WEBSOCKET CONNECTIONS
-  
-  var WebSocket = new ws({server:Server})
-
-  WebSocket.on('connection', HandleSoc)
-
-  function HandleSoc (soc) { // fix this thing!
-    var wss = websocketStream(soc)
-    var headers = soc.upgradeReq.headers
-    var key = headers['sec-websocket-key']
-    if (headers['sec-websocket-key1']) key = headers['sec-websocket-key1'].replace(/\s/g,'-')
-    var host = headers.host
-
-    _[host].addSocket(key, function socketAdded() {
-      console.log('opened socket: '+key+' to brico: '+host)
-      wss.pipe(_[host][key])
-      wss.write(JSON.stringify({'api':['map',MAP]}))
-    })
-  }
-
-  // API
-   
-  var API = {
-    load: function (u, cb) {
-      console.log('loading...')
-      var streamBricos = Data.createValueStream()
-      streamBricos.on('data', function (d) {
-        var brico = JSON.parse(d)
-        _[brico.host] = new Bricoleur()
-        _[brico.host].data = level(opts.path_data+brico.host)
-      })
-      streamBricos.on('end', function () {
-        cb('done!')
-      })
-    },
-    createBrico: function (brico, next) {
-      Data.put(brico.key, JSON.stringify(brico), function () {
-        console.log(brico)
-        next(brico)
-      }) 
-    }
-  }
-  this.api = new fern({tree:API})
 }
