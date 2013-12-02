7752434fe98c3f90d6d513b6cb09ce72e20f2d54
nimoy <ag@theblacksea.cc>
2013-12-02 12:45:21 -0700

    map reads standard package.json

--- _map.js
+++ _map.js
@@ -1,70 +1,31 @@
 // WILDS MAPPER
 
-// this should work with standard module format + 
-// package.json files
-// how to nicely extend / hack the package.json
-
-var Readable = require('stream').Readable
 var asyncMap = require('slide').asyncMap
 var fs = require('fs')
 
-// read through dir + look for specified package.json property
-// use property to set behaviour + module scope
-
 module.exports = Map
 
-function Map (opts, mapStream) {
-  if (opts.path_wilds[opts.path_wilds.length-1] !== '/') opts.path_wilds += '/'
-  var FileStat 
+function Map (path, ready) {
+  if (path[path.length-1] !== '/') path += '/'
 
-  var s = new Readable({end:false})
-  s._read = function () {} // WTF!
+  var MAP = {}
 
-  fs.watch(opts.path_wilds, function handleFileChange (event, file) { 
-    var filepath = opts.path_wilds+file
-    fs.stat(filepath, function statFile (e, stats) {
-      if (e) console.error(e)
-      stats.filepath = filepath
-      if (!FileStat) FileStat = stats
-      if (FileStat.size !== stats.size) {
-        var mod = stats.filepath.split('/')[2]
-        var ext = mod.split('.')[1]
-        var file = mod
-        if (ext !== 'html') Fresh = true
-        if (ext !== 'js') file = mod.split('.')[0]+'.js' 
-        Parse(file, function () {
-          console.log('updated '+ stats.filepath)
-        })
-      } 
-      FileStat = stats
+  function readPKG (fileName, next) {
+    fs.readFile(path+fileName+'/package.json', function (e, buf) {
+      if (e) {
+        console.error(e)
+      }
+      if (!e) {
+        var jsn = JSON.parse(buf)
+        MAP[fileName] = jsn
+        next() 
+      }
     })
-  })
- 
-  function Parse (file, next) {
-    var ext = file.split('.')[1]
-    if (ext === 'js' && file[0] !=='_') {
-      var f = fs.createReadStream(opts.path_wilds+file)
-      f.on('data', function (chunk) {
-        var buf = chunk.toString()
-        var m = buf.match(/\/\*\{([\S\s]*)\}\*\//)
-        var modJSON = m[0].replace('/*','').replace('*/','')
-        if (Fresh === true) {
-          var mod = JSON.parse(modJSON)
-          mod.fresh = true
-          modJSON = JSON.stringify(mod)
-        }
-        s.push(modJSON)
-      })
-      f.on('end', next) 
-    } else {
-      next()
-    }
   }
 
-  fs.readdir(opts.path_wilds, function handleWildsFiles (e, files) {
-    mapStream(s)
-    asyncMap(files, Parse, function doneWildsFiles () {
-      s.emit('end')
+  fs.readdir(path, function moduleList(e, modules) {
+    asyncMap(modules, readPKG, function () {
+      ready(JSON.stringify(MAP,null,2))
     })
   })
 }
