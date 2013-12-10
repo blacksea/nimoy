30b8bcdaaec1ce20a50b8fc673f0c13a41d49390
nimoy <ag@theblacksea.cc>
2013-12-09 19:23:12 -0700

    add fs canvas, window dim & proper paper loading to ui

--- _wilds/ui/index.js
+++ _wilds/ui/index.js
@@ -1,16 +1,40 @@
 // UI ELEMENTS with PAPERJS
 
-var paperJS = document.createElement('script')
-paperJS.setAttribute('src','./paper.min.js')
+var through = require('through')
 
-var cvs = document.createElement('canvas')
-cvs.width = document.client.width
-cvs.height = document.client.height
+var winHeight = document.body.clientHeight
+var winWidth = document.body.clientWidth
+var winCenterX = winWidth / 2
+var winCenterY = winHeight / 2
 
+window.onresize = function () {
+  winHeight = document.body.clientHeight
+  winWidth = document.body.clientWidth
+  winCenterX = winWidth / 2
+  winCenterY = winHeight / 2
+  cvs.width = winWidth
+  cvs.height = winHeight
+}
+
+// SETUP BODY
 var body = document.body
+body.style.margin = 0
+body.style.padding = 0
+
+// INSTALL CANVAS & MAKE IT FULLSCREEN WITH AUTO RESIZE
+var cvs = document.createElement('canvas')
+cvs.setAttribute('resize','')
+cvs.style.backgroundColor = '#f1f1f1'
+cvs.width = winWidth
+cvs.height = winHeight
 body.appendChild(cvs)
 
-paperJS.onload = function () {
-  console.log(paper)
-  paper.setup(cvs)
+// INSTALL PAPER
+var paperJS = document.createElement('script')
+paperJS.setAttribute('src','/paper-core.min.js')
+body.appendChild(paperJS)
+paperJS.addEventListener('load',init,false)
+
+function init () {
+
 }
