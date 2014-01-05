28952dcb892c5d6a18f2908670fe35592a1fb607
nimoy <ag@theblacksea.cc>
2014-01-04 23:15:43 -0700

    fix ondata ?

--- hook.js
+++ hook.js
@@ -3,14 +3,16 @@ var port = 80
 var host = 'theblacksea.cc'
 
 var server = http.createServer(function  (req,res) {
-  if (req.method === 'POST') {
-    if (req.headers.host === 'git.'+host) console.log('post to git subdomain')
+  if (req.method === 'POST' && req.headers.host === 'git.'+host) {
+    console.log('post to git subdomain')
+    var data = ''
     req.on('data', function (d) {
-      console.log(d)
-      console.log(d.toString())
+      data += d.toString()
     })
     req.on('end', function () {
+      console.log(data)
       console.log(req.headers)
+      res.end()
     })
   } else {
     res.statusCode = 404
