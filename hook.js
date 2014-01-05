e5c5fd53ef9ffc9876ea41b3cbc7b4be77b81835
nimoy <ag@theblacksea.cc>
2014-01-04 22:59:31 -0700

    fix typo

--- hook.js
+++ hook.js
@@ -2,7 +2,7 @@ var http = require('http')
 var port = 80
 var host = 'theblacksea.cc'
 
-var server = http.createServer (req,res) {
+var server = http.createServer(function  (req,res) {
   if (req.method === 'post' && req.headers.host = 'git.'+host) {
     req.on('data', function (d) {
       console.log(d)
@@ -14,7 +14,7 @@ var server = http.createServer (req,res) {
     res.statusCode = 404
     res.end('error 404')
   }
-}
+})
 server.listen(port,host, function () {
   console.log('listening on port: '+port+' and host: '+host)
 })
