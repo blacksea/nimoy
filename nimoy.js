29bce7b98a9f0a0dd18608512f979ca856a32d62
nimoy <ag@theblacksea.cc>
2014-01-06 23:10:12 -0700

    move repl into nimoy

--- nimoy.js
+++ nimoy.js
@@ -130,3 +130,44 @@ function WS (port, cb) {
     }
   })
 }
+
+// REPL
+var clc = require('cli-color')
+var through = require('through')
+var read = require('read')
+
+function REPL (msg) {
+  var colors = [
+    {f:0,b:11},
+    {f:0,b:14},
+    {f:0,b:15}
+  ]
+
+  var clr = colors[Math.floor(Math.random() * ((colors.length-1) - 0 + 1) + 0)]
+
+  if (msg) console.log(clc.xterm(clr.b)(msg))
+  read({}, function handleInput (e,c,d) {
+    if (e) console.error(e)
+    if (!e) {
+      var args = c.match(' ')
+      if (args !== null) { 
+        c = c.split(' ')
+        s.write(c)
+        REPL()
+      } else {
+        s.write(c)
+        REPL()
+      }
+    }
+  })
+
+  var s = through(function write (d) {
+    this.emit('data',d) 
+  }, function end () {
+    this.emit('end')
+  })
+
+  return s
+}
+
+module.exports = REPL
