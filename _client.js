80a83746b8d946c122d1b9f68734c92ee8971255
nimoy <ag@theblacksea.cc>
2014-01-21 20:15:07 -0700

    set socket based on protocol

--- _client.js
+++ _client.js
@@ -1,1 +1,5 @@
 var level = require('multilevel')
+var websocStream = require('websocket-stream')
+var host = window.document.location.host.replace(/:.*/, '')
+if (window.location.protocol && window.location.protocol==='https:') var ws = websocStream('wss://'+host)
+if (window.location.protocol && window.location.protocol==='http:') var ws = websocStream('ws://'+host)
