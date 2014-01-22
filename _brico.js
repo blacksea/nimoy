c0784cdf781f722f3e619d1e55190f9088a933c9
nimoy <ag@theblacksea.cc>
2014-01-21 19:38:22 -0700

    add test placeholder + split brico into server/client

--- _brico.js
+++ _brico.js
@@ -1,12 +1,12 @@
 // BRICO
-var level = require('level')
-var db = level('./data') // username?
 
-// this should only run server side
+// SERVER SIDE CODE 
+var level = require('level')
+
 function mapWilds (wilds, fin) {
+  var MAP = {}
   var fs = require('fs')
   var asyncMap = require('slide').asyncMap
-  var MAP = {}
   function readPkg (modDir, next) {
     var pkg = JSON.parse(fs.readFileSync(wilds+modDir+'/package.json'))
     if (pkg.brico) { 
@@ -19,43 +19,38 @@ function mapWilds (wilds, fin) {
   })
 }
 
+// UNIVERSAL
 module.exports = function bricoleur (opts) {
+  var db = level('./'+opts.name)
   var self = this
-
   this.find = function (string, cb) { // search map for module
 
   }
-
   this.put = function (mod, cb) { // put module
     // put 'module' opt=string opt=string
 
   }
-
   this.rm = function (mod, cb) { // rm module
     // rm module
 
   }
-
   this.conn = function (mods, cb) { // connect modules
     // conn module module module
 
   }
-
-  this.disConn = function (mods, cb) { // disconnect modules
+  this.disconn = function (mods, cb) { // disconnect modules
     // disconn module /single /chain
 
   }
-
   this.status = function (cb) {
     // view env & conns
+    
   }
-
   // map / survey / library -- transforms?
   // search
   // put / rm
   // conn / disconn
   // env / status
   // events
-  
   // object/transport/stream protocol
 }
---
+++ tests/0.js
@@ -0,0 +1,7 @@
+// test brico
+
+// run live with nimoy + cli
+
+var brico = require('../_brico.js')
+
+brico = new brico()
