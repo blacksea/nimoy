6619a43c99279c972b08d2ed092b1c0bcf599279
nimoy <ag@theblacksea.cc>
2014-01-22 22:33:50 -0700

    rough in simple test

--- tests/0.js
+++ tests/0.js
@@ -1,9 +1,18 @@
-// test brico
-
-// run live with nimoy + cli
+var fs = require('fs')
 var map = require('../_map')
 var test = require('tape')
 
-map('../_wilds/', function (m) {
-  console.log(m)
+var dir = '../_wilds/'
+var mods
+
+fs.readdir(dir, function (e,m) {
+  if(e)console.error(e)
+  if(!e)mods = m
+})
+
+test('map test', function (t) {
+  map(dir, function (m) {
+    t.equal((typeof m),'object','typeof map is object')
+    t.end()
+  })
 })
