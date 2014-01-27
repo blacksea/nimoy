c131fb73ef4a35996aa72cad26732dade69c9f19
nimoy <ag@theblacksea.cc>
2014-01-27 11:53:50 -0700

    added filter to brico liveStream

--- _brico.js
+++ _brico.js
@@ -3,36 +3,40 @@
 // * brico replicates to client nodes --- client node can have different access priveleges
 // modules need an interface/spec --- pass in opts and return stream for now
 // some modules may need time to load / init
-// expose module data streams
 // map / survey / library -- transforms?
 // search
 // put / rm
 // conn / disconn
 // env / status
-// events
 // object/transport/stream protocol
 
-var env = process.title // node or browser
+var through = require('through')
+
+var proc = process.title // node or browser
 
 module.exports = function bricoleur (data) {
   var self = this
 
   var liveStream = data.liveStream({old:false}) 
 
-  liveStream.on('data', handleData)
+  liveStream.pipe(through(function handleData (d) {
+    var val
+    var key = d.key
+    if (typeof d.value === 'string' && d.value[0] === '{') val = JSON.parse(d.value)
 
-  function handleData (d) {
+    if (filter[key]) filter[key](val)
 
-    // old
-    if (!d.type) { 
-      // {key: val:}
+
+  }, function end () {
+    this.end()
+  }))
+
+  var filter = {
+    map : function (m) {
+      console.log(m)
+    },
+    env : function (env) {
+      console.log(env)
     }
-
-    // fresh
-    if (d.type === 'put') {
-      // {type: key: val: }
-      
-    } 
-
   }
 }
--- _client.js
+++ _client.js
@@ -4,8 +4,8 @@
 var websocStream = require('websocket-stream')
 var host = window.document.location.host.replace(/:.*/, '')
 if (window.location.port) host += (':' + window.location.port)
-if (window.location.protocol === 'https:') var ws = websocStream('wss://'+host)
-if (window.location.protocol === 'http:') var ws = websocStream('ws://'+host)
+if (window.location.protocol === 'https:') var ws = websocStream('wss://' + host)
+if (window.location.protocol === 'http:') var ws = websocStream('ws://' + host)
 
 // SETUP DB
 var ml = require('multilevel')
