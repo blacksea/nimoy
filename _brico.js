d5eae9f7cf7523e31254c10aafaa05a46ca83a23
nimoy <ag@theblacksea.cc>
2014-01-27 03:54:32 -0700

    use uglify js

--- _brico.js
+++ _brico.js
@@ -2,66 +2,36 @@
 
 // * brico replicates to client nodes --- client node can have different access priveleges
 // modules need an interface/spec --- pass in opts and return stream for now
-
 // some modules may need time to load / init
 // expose module data streams
+// map / survey / library -- transforms?
+// search
+// put / rm
+// conn / disconn
+// env / status
+// events
+// object/transport/stream protocol
 
 var env = process.title // node or browser
 
 module.exports = function bricoleur (data) {
   var self = this
 
-  // leveldb live events
   var liveStream = data.liveStream() 
+
   liveStream.on('data', handleData)
 
   function handleData (d) {
+
     // old
     if (!d.type) { 
-
+      // {key: val:}
     }
+
     // fresh
     if (d.type === 'put') {
-      switch (d.key) {
-        case 'map' : handleMap(d.value); break;
-      }
+      // {type: key: val: }
     } 
-  }
-
-  function handleMap (m) {
-    var map = JSON.parse(m)
-    console.log(map)
-  }
-
-  this.put = function (mod, cb) { // put module
-    // put 'module' opt=string opt=string
 
   }
-
-  this.rm = function (mod, cb) { // rm module
-    // rm module
-
-  }
-
-  this.conn = function (mods, cb) { // connect modules
-    // conn module module module
-
-  }
-
-  this.disconn = function (mods, cb) { // disconnect modules
-    // disconn module /single /chain
-
-  }
-
-  this.status = function (cb) {
-    // view env & conns
-    
-  }
-  // map / survey / library -- transforms?
-  // search
-  // put / rm
-  // conn / disconn
-  // env / status
-  // events
-  // object/transport/stream protocol
 }
