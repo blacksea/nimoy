dac60c5d06f4649d9448737fd6aa48b88cacbb31
nimoy <ag@theblacksea.cc>
2014-02-08 00:43:07 -0700

    mv _repl to _cli

--- nimoy.js
+++ nimoy.js
@@ -66,7 +66,7 @@ function BOOT () {
     })
 
     if (config.repl === true) {
-      var repl = require('./_repl')(db) // REPL : pipes into db.writeStream
+      var repl = require('./_cli')(db) // REPL : pipes into db.writeStream
       process.stdin.pipe(repl).pipe(process.stdout)
     }
   })
