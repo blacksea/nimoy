f732c23b359d2063644d441a58825750bee7b77a
nimoy <ag@theblacksea.cc>
2014-02-12 13:37:27 -0700

    added test for _brico

---
+++ tests/brico.js
@@ -0,0 +1,11 @@
+var test = require('tape')
+var level = require('level')
+var liveStream = require('level-live-stream')
+var db = level('testDB')
+liveStream.install(db)
+
+var bricoleur = require('../_brico.js')
+var brico = new bricoleur(db)
+
+
+
