02653b0516f39ce9606746cfb96cf73cd938cfaa
nimoy <ag@theblacksea.cc>
2014-02-12 13:31:24 -0700

    refactoring brico, using wilds for spaces, remove weird filter tree

--- _brico.js
+++ _brico.js
@@ -4,72 +4,58 @@ var through = require('through')
 var fern = require('fern')
 var conf = require('./__conf.json') 
 var proc = process.title // node or browser
+var interface
 
-// SPACES
-// * = Module
-// ^ = Library
-// # = Connection
-// _ = Process
+var WILDS = {}
 
-var filter = {
-  put: {
-    make: function (d) { 
-      var m = d.key.split(':')[1]
-      var mPath = config.dir_wilds+m
-      _[m] = require(mPath)(d.value)
-    },
-    conn: function (d) {
-    }
-  },
-  del: {
-    make: function (d) {
-      var m = d.key.split(':')[1]
-      _[m].emit('close')
-      delete _[m]
-    },
-    conn: function (d) {
-    }
-  }
+WILDS['*'] = function (i,o) { // * MODULE
+
 }
-module.exports.filter = filter
+WILDS['^'] = function (i,o) { // ^ LIBRARY
 
+}
+WILDS['#'] = function (i,o) { // # CONNECT
+
+}
+WILDS['_'] = function (i,o} { // _ PROCESS
+
+}
+
+interface = through(function write (d) {
+  var path = key.split(':')
+  var space = path[0]
+
+}, function end () {
+  this.emit('end')
+}, {autoDestroy:false})
 
 module.exports = function bricoleur (data) { 
-  var _ = {}
   
-  var fil = fern(filter)
-  fil.on('error', console.error)
   var liveStream = data.liveStream({old:false}) 
-  liveStream.pipe(fil)
-
-
-  // UTIL
-  function search (args, cb) {
-    var match = false
-    var ks = data.createKeyStream()
-    ks.on('data', function (d) {
-      var path = d.split(':')
-      if (args[1] === path[1]) {
-        match = true
-        for (var i=2; i<args.length; i++) {
-          var pair = args[i].split('=')
-          var key = pair[0]
-          var val = pair[1]
-          opts[key] = val
-        }
-        if (opts === {}) opts = null
-      }
-    })
-    ks.on('end', function () {
-      if (match !== true) interface.emit('error', new Error('could not find module'))
-    })
-  }
-
-  var interface = through(function write (d) {
-    this.emit('data', d)
-  }, function end () {
-    this.emit('end')
-  })
 
   return interface
+}
+
+
+// UTIL
+
+function search (args, cb) {
+  var match = false
+  var ks = data.createKeyStream()
+  ks.on('data', function (d) {
+    var path = d.split(':')
+    if (args[1] === path[1]) {
+      match = true
+      for (var i=2; i<args.length; i++) {
+        var pair = args[i].split('=')
+        var key = pair[0]
+        var val = pair[1]
+        opts[key] = val
+      }
+      if (opts === {}) opts = null
+    }
+  })
+  ks.on('end', function () {
+    if (match !== true) interface.emit('error', new Error('could not find module'))
+  })
 }
