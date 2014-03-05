6fe8f5e37472cdd79627a59fe6a906d7890dcc34
nimoy <ag@theblacksea.cc>
2014-03-04 17:09:41 -0700

    change _map to an eventEmitter

--- nimoy.js
+++ nimoy.js
@@ -1,32 +1,29 @@
-// NIMOY
+// NIMOY  
 
 var fs = require('fs')
 var clc = require('cli-color')
 var log = clc.cyanBright
 var err = clc.red
+var level = require('level')
+var multilevel = require('multilevel')
+var liveStream = require('level-live-stream')
 
 
-// CONFIG 
-
+// CONFIGURATION  
 var config
 !process.argv[2] ? config = require('./__conf.json') : config = require(process.argv[2])
 if (config.dirStatic[config.dirStatic.length-1] !== '/') config.dirStatic += '/'
 if (config.dirModules[config.dirModules.length-1] !== '/') config.dirModules += '/'
-    
 
-// SETUP DB
- 
-var level = require('level')
-var multilevel = require('multilevel')
-var liveStream = require('level-live-stream')
+
+// SETUP MULTILEVEL
 var db = level('./'+config.host) 
 liveStream.install(db)
-multilevel.writeManifest(db, config.dirStatic + '/manifest.json')
+multilevel.writeManifest(db, config.dirStatic+'/manifest.json')
 
-
-// GENERATE BROWSER CODE 
+1// GENERATE BROWSER CODE 
 fs.writeFileSync(config.dirStatic+'boot.js', functionToString(function () {
-// Start Browser Boot
+// Start Browser Boot 
 var websocStream = require('websocket-stream')
 var host = windowBdocument.location.host.replace(/:.*/, '')
 if (window.location.port) host += ':'+window.location.port
@@ -49,40 +46,37 @@ brico.on('error', function (e) {
 })) 
 
 
-// RUN MAP / BROWSERIFY 
+// LOAD BRICOLEUR
+var bricoleur = require('./bricoleur')
+brico = bricoleur(db) 
+brico.on('error', console.error)
 
-var dbMapStream = db.createWriteStream({type:'put'})
-var map = require('./_map')({
+
+// LOAD MAP
+var map = require('./_map')({ // 
   wilds : config.dirModules,
   bundle : config.dirStatic+'bundle.js',
   browserify: config.dirStatic+'boot.js',
   min : config.minify
 })
-map.pipe(dbMapStream)
-dbMapStream.on('close', BOOT)
-             
+map.on('map', db.put)
+map.on('end', BOOT)
+map.on('error', console.error)
+
 
 // BOOT SERVER
-
 function BOOT () {
   var stat = fs.statSync(config.dirStatic+'bundle.js')
   console.log(log('wrote bundle ('+(stat.size/1024).toFixed(2)+'/kb) to '+config.dirStatic+'bundle.js'))
-
   // RUN BRICO  
-  var bricoleur = require('./bricoleur')
-  brico = bricoleur(db) 
-  brico.on('error', console.error)
-
   bootnet(function () {
     console.log(log('network running on port: '+config.port+' host: '+config.host))
-  
     if (config.cli === true) {
       var cli = require('./_cli')()
       process.stdin.pipe(cli).pipe(brico).pipe(process.stdout)
     }
   })
 }
-
 function bootnet (booted) {
   var http = require('http')
   var https = require('https')
@@ -103,9 +97,7 @@ function bootnet (booted) {
       ciphers: 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS'
     }, handleRequests)
   } 
-
   server.listen(config.port, config.host, installWS)
-
   function handleRequests (req, res) { // more robust: needs paths as well as files
     var url = req.url.substr(1)
     if (url === '') {
@@ -123,14 +115,12 @@ function bootnet (booted) {
       })
     }
   }
-
   function installWS () {
     var webSocketServer = require('ws').Server
     var ws = new webSocketServer({server:server})
     ws.on('connection', handleSoc)
     booted() // NETWORK READY
   }
-
   function handleSoc (soc) {
     var headers = soc.upgradeReq.headers
     var id = headers['sec-websocket-key']
@@ -144,7 +134,6 @@ function bootnet (booted) {
     })
   }
 }
-
 
 // UTILS
 function functionToString (fn) {// takes fn as input, unwraps and returns string
