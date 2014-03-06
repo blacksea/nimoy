6cf11f3a9edca6bbe71e0046feca73ce32bcc8a0
nimoy <ag@theblacksea.cc>
2014-03-05 23:38:51 -0700

    change db.createRpc to ml.createRpc

--- nimoy.js
+++ nimoy.js
@@ -30,14 +30,14 @@ fs.writeFileSync(config.dirStatic+'boot.js', functionToString(function () {
   
 var websocStream = require('websocket-stream')
 var host = window.document.location.host.replace(/:.*/, '')
-if (window.location.port) host += ':'+window.location.port
+if (window.location.port) host += (':'+window.location.port)
 if (window.location.protocol === 'https:') var ws = websocStream('wss://' + host)
 if (window.location.protocol === 'http:') var ws = websocStream('ws://' + host)
 
 var ml = require('multilevel')
 var manifest = require('./manifest.json')
 var multiLevel = ml.client(manifest)
-var rpc = db.createRpcStream()
+var rpc = ml.createRpcStream()
 ws.pipe(rpc).pipe(ws)
 
 var bricoleur = require('../bricoleur')
@@ -66,7 +66,9 @@ var map = require('./_map')({
   browserify: config.dirStatic+'boot.js',
   min : config.minify
 })
-map.on('map', db.put)
+map.on('map', function (key,val) {
+  db.put(key,val)
+})
 map.on('end', function () {
  var stat = fs.statSync(config.dirStatic+'bundle.js')
   console.log(log('wrote bundle ('+(stat.size/1024).toFixed(2)+'/kb) to '+config.dirStatic+'bundle.js'))
@@ -86,13 +88,13 @@ var file = new static.Server(config.dirStatic)
 
 function HandleRequests (req, res) { 
   var secure = req.connection.encrypted 
+  if (secure===true) res.setHeader('Strict-Transport-Security','max-age=31536000')
 
-  req.addListener('end', function serveFile () {
-    if (secure===true) res.setHeader('Strict-Transport-Security','max-age=31536000')
-    file.serve(req, res, handlePath)
-  })
+  file.serve(req, res, handlePath)
+
   function handlePath (e, result) {
     if (e) res.end('404')
+    if (!e) console.log(result)
     // res.setHeader('content-type','text/html')
     // if (secure===true) res.setHeader('Strict-Transport-Security','max-age=31536000')
     // res.end('<html><head></head><body><script src="/bundle.js"></script></body></html>')
@@ -111,9 +113,9 @@ function InstallWebsocket () {
   })
 }
 
-var server = require('http').createServer(HandleRequests)
+var server
 
-var server = require('https').createServer({
+!config.crypto ? server = require('http').createServer(HandleRequests) : server = require('https').createServer({
   key: fs.readFileSync(config.crypto.key),
   cert: fs.readFileSync(config.crypto.cert),
   honorCipherOrder: true,
@@ -121,7 +123,7 @@ var server = require('https').createServer({
   ciphers: 'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS'
 }, HandleRequests)
 
-server.listen(config.port, config.host, installWS)
+server.listen(config.port, config.host, InstallWebsocket)
 //////////////////////////////////////////////////
 
 // UTILS
