ae07af5e739c25a0f424e31906b6f105a481f976
nimoy <ag@theblacksea.cc>
2014-03-07 03:54:56 -0700

    fix map write stream

--- nimoy.js
+++ nimoy.js
@@ -5,6 +5,7 @@ var liveStream = require('level-live-stream')
 var bricoleur = require('./bricoleur')
 var fileServer = require('node-static').Server
 var webSocketStream = require('websocket-stream')
+var webSocketServer = require('ws').Server
 
 var server
 var brico
@@ -17,16 +18,18 @@ if (config.dirModules.slice(-1) !== '/') config.dirModules += '/'
 if (config.dirStatic.slice(-1) !== '/') config.dirStatic += '/'
 
 if (config.crypto) {
+
   var cipher = 'ecdh+aesgcm:dh+aesgcm:ecdh+aes256:dh+aes256:'
   + 'ecdh+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:'
   + 'RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS'
 
-  config.crypto.key = fs.readfilesync(config.crypto.key)
-  config.crypto.cert = fs.readfilesync(config.crypto.cert)
-  config.crypto.honorcipherorder = true
-  config.crypto.ciphers = cipher
+  server = require('https').createServer({
+    key : fs.readfilesync(config.crypto.key)
+    cert : fs.readfilesync(config.crypto.cert)
+    honorcipherorder : true
+    ciphers : cipher
+  }, HandleRequests)
 
-  server = require('https').createServer(config.crypto, HandleRequests)
   file = fileServer(config.dirStatic, {'Strict-Transport-Security':'max-age=31536000'})
 
 } else {
@@ -40,21 +43,23 @@ function HandleRequests (req, res) {
   })
 }
 
-function setupWebSocket () {
-  var webSocketServer = require('ws').Server
+server.listen(config.port, config.host, function setupWebSocket () {
   var ws = new webSocketServer({server:server})
+
   ws.on('connection', function handleSoc (soc) {
     var levelServer = multiLevel.server(db)
     var wss = webSocketStream(soc) 
+
     wss.pipe(levelServer).pipe(wss)
-    brico.installMuxDemux(levelServer)
+
     wss.on('error', function (e) {
       console.error('webSocStreamErr: '+e)
     })
-  })
-}
 
-server.listen(config.port, config.host, setupWebSocket)
+    brico.installMuxDemux(levelServer)
+  })
+})
+
 
 var db = level('./'+config.host) 
 liveStream.install(db)
@@ -64,60 +69,62 @@ brico = bricoleur(db)
 brico.on('error', console.error)
 
 
-// Write boot.js
-var indexHtml = '<!doctype html><html lang="en">'
-  + '<head><meta charset="utf-8"></head>'
-  + '<body><script src="/bundle.js"></script></body>'
-  + '</html>'
-
-fs.writeFileSync(config.dirStatic+'index.html', indexHtml)
- 
-fs.writeFileSync(config.dirStatic+'boot.js', functionToString(function () {
-// Start Browser Boot 
-var websocStream = require('websocket-stream')
-var host = window.document.location.host.replace(/:.*/, '')
-if (window.location.port) host += (':'+window.location.port)
-if (window.location.protocol === 'https:') var ws = websocStream('wss://' + host)
-if (window.location.protocol === 'http:') var ws = websocStream('ws://' + host)
-
-var ml = require('multilevel')
-var manifest = require('./manifest.json')
-var multiLevel = ml.client(manifest)
-var rpc = multiLevel.createRpcStream()
-ws.pipe(rpc).pipe(ws)
-ws.on('error', function (e) {
-  console.error(e)
-})
-
-var bricoleur = require('../bricoleur')
-var brico = bricoleur(multiLevel)
-brico.installMuxDemux(rpc)
-brico.on('error', function (e) {
-  console.error(e)
-})
-// End Browser Boot
-})) 
+generateStaticFiles (function mapAndBrowserify () {
+  var map = require('./_map')({
+    wilds : config.dirModules,
+    bundle : config.dirStatic+'bundle.js',
+    browserify: config.dirStatic+'boot.js',
+    min : config.minify
+  })
+  map.on('error', console.error)
+  map.on('data', function (d) {
+    db.put('^',d) 
+  })
+  map.on('end', function () {
+    // console.log(log('wrote bundle ('+(stat.size/1024).toFixed(2)+'/kb) to '+config.dirStatic+'bundle.js'))
+    console.log('nimoy running on host: "'+config.host+'" port: "'+config.port+'"')
+    if (config.cli === true) process.stdin.pipe(require('./_cli')()).pipe(brico).pipe(process.stdout)
+  })
+}
 
 
-var map = require('./_map')({  // make this a stream
-  wilds : config.dirModules,
-  bundle : config.dirStatic+'bundle.js',
-  browserify: config.dirStatic+'boot.js',
-  min : config.minify
-})
-map.on('error', console.error)
-map.on('data', function (d) {
-  db.put('^',d) 
-})
-map.on('end', function () {
-  // console.log(log('wrote bundle ('+(stat.size/1024).toFixed(2)+'/kb) to '+config.dirStatic+'bundle.js'))
-  console.log('nimoy running on host: "'+config.host+'" port: "'+config.port+'"')
-  if (config.cli === true) process.stdin.pipe(require('./_cli')()).pipe(brico).pipe(process.stdout)
-})
+function generateStaticFiles (end) {
 
+  var indexHtml = '<!doctype html><html lang="en">'
+    + '<head><meta charset="utf-8"></head>'
+    + '<body><script src="/bundle.js"></script></body>'
+    + '</html>'
 
+  fs.writeFileSync(config.dirStatic+'index.html', indexHtml)
+   
+  fs.writeFileSync(config.dirStatic+'boot.js', functionToString(function () {
+  // Start Browser Boot 
+  var websocStream = require('websocket-stream')
+  var host = window.document.location.host.replace(/:.*/, '')
+  if (window.location.port) host += (':'+window.location.port)
+  if (window.location.protocol === 'https:') var ws = websocStream('wss://' + host)
+  if (window.location.protocol === 'http:') var ws = websocStream('ws://' + host)
 
+  var ml = require('multilevel')
+  var manifest = require('./manifest.json')
+  var multiLevel = ml.client(manifest)
+  var rpc = multiLevel.createRpcStream()
+  ws.pipe(rpc).pipe(ws)
+  ws.on('error', function (e) {
+    console.error(e)
+  })
 
+  var bricoleur = require('../bricoleur')
+  var brico = bricoleur(multiLevel)
+  brico.installMuxDemux(rpc)
+  brico.on('error', function (e) {
+    console.error(e)
+  })
+  // End Browser Boot
+  })) 
+
+  end()
+}
 
 
 // Utils
