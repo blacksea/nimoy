ba9a417d005ee0dfbcae6b9fd894b742657f3eb4
nimoy <ag@theblacksea.cc>
2014-03-14 05:01:10 -0600

    move api into bricoleur

--- _bricoleur.js
+++ _bricoleur.js
@@ -1,5 +1,5 @@
-var api = require('./_api')
 var fern = require('fern')
+var through = require('through')
 var proc = process.title
 
 function Bricoleur (multiLevel, opts) { 
@@ -100,8 +100,93 @@ function Bricoleur (multiLevel, opts) {
     if (proc === 'browser') muxDemux.on('connection', newMuxConn)
   } 
 
+  var api = through(function Write (d) {
+    var cmd
 
-  this.api = api(multiLevel, {})
+    if (typeof d === 'string') cmd = d.split(' ')
+
+    if (cmd.length === 1) {
+
+      if (cmd[0].match(/\|/)) { // pipe
+        var name = cmd[0]
+        var uid = new Date().getTime()
+        var key = '#:'+name+':'+uid
+        var value = cmd[0]
+        multiLevel.put(key, value, handleRes)      
+      }
+
+      if (cmd[0].match('-') !== null) { // unpipe
+        multiLevel.del(cmd[0], handleRes)
+      }
+
+      if (cmd[0] === 'ls')  {
+        // create active map --- write stream of active modules
+      }
+
+    } 
+
+    if (cmd[0] === 'put') {
+      var name = cmd[1]
+      var uid = new Date().getTime()
+      var key = '*:'+name+':'+uid
+
+      var value = (!cmd[2]) 
+        ? {}
+        : JSON.parse(cmd[2])
+
+      multiLevel.put(key, value, handleRes)
+    }
+
+    if (cmd[0] === 'del') {
+      var uid = cmd[1]
+      multiLevel.del(key, value, handleRes)
+    }
+
+    if (cmd[0] === 'search') {
+      var str = cmd[1]
+      search(str, function result (e, res) {
+        if (e) api.emit('error', e)
+        if (!e) api.emit('data', {
+          type: 'search',
+          status:1,
+          val: res
+        })
+      })
+    }
+
+  }, function End () {
+
+    this.emit('end')
+
+  })
+
+
+  function search (str, res) {
+    var result
+
+    multiLevel.createKeyStream()
+      .on('data', function (key) {
+        var items = key.split(':')
+        for (var i=0;i < items.length;i++) {
+          if (items[i] === str) {
+            result = key
+            res(null, result)
+            break;
+          }
+        }
+      })
+      .on('close', function () {
+        if (!result) res(new Error('not found'), null)
+      })
+  }
+
+
+  function handleRes (e) {
+    if (e) api.emit('error', e)
+  }
+
+
+  this.api = api
 
 } 
 
