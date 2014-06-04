a2f97458ff8f6b4c80cb5a78428ef01e67394563
nimoy <ag@theblacksea.cc>
2014-06-04 11:50:54 -0700

    begin nimoy refactor

--- nimoy.js
+++ nimoy.js
@@ -1,19 +1,28 @@
 var fs = require('fs')
+var url = require('url')
+var http = require('http')
 var level = require('level')
-var multiLevel = require('multilevel')
-var liveStream = require('level-live-stream')
-var fileServer = require('node-static')
-var webSocketStream = require('websocket-stream')
-var webSocketServer = require('ws').Server
-var Bricoleur = require('./_bricoleur')
-var mappify = require('./_map')
+var stache = require('templayed')
+var config = require('./config.json')
+var multilevel = require('multilevel')
+var formidable = require('formidable')
+var browserify = require('browserify')
+var livestream = require('level-live-stream')
+var engineserver = require('engine.io-stream')
 
-var server
-var serverInfo = 'nginx' // uhm...
-var brico
-var file
-var dB
+var newhmac = require('crypto').createhmac
+var key = 'italocalvino'
+var algo = 'sha256'
+var modes = {}
 
+var index = './static/index.html'
+fs.watchfile(config.components+'index.hogan', makeindex)
+
+var db = level('./data')
+livestream.install(db)
+multilevel.writemanifest(db, './static/manifest.json')
+
+var update = db.livestream()
 
 var config = (process.argv[2]) 
   ? config = require(process.argv[2]) 
@@ -22,7 +31,45 @@ var config = (process.argv[2])
 if (config.modules.slice(-1) !== '/') config.modules += '/' 
 if (config.static.slice(-1) !== '/') config.static += '/'
 
-var mount = st({index:'index.html',path:config.static,passthrough:true}) 
+// configuration
+if (config.brico) {
+  var conf = config.brico
+  for (m in conf.modes) {
+    if (conf.modes[m].pass) {
+      var token = conf.modes[m].pass
+      delete conf.modes[m].pass
+      gethmac({token:token,user:m}, function (d) { modes[d.key] = d.val })
+    }
+  }
+  db.put('config', json.stringify(conf))
+}
+
+// server!
+var fileserver = require('node-static').server
+var file = new fileserver('./static')
+
+var server = http.createserver(function (req, res) {
+  if (req.url === '/upload' && req.method === 'post') fileupload(req, res)
+
+  file.serve(req, res, function noFile (e) {
+    if (e) {
+      var path = url.parse(req.url).pathname.slice(1)
+      file.serveFile('/index.html', 200, {}, req, res)
+      console.log(path)
+    }
+  })
+})
+    
+var engine = engineServer(function (wss) {
+  wss.pipe(multiLevel.server(db, {auth: Auth, access: Access})).pipe(wss)
+  wss.on('error', console.error)
+}, {cookie:false})
+
+engine.attach(server, '/ws')
+
+server.listen(config.port, config.host, function () {
+  console.log('server running on port: '+config.port+' host: '+config.host)
+})
 
 if (config.crypto) {
 
@@ -44,104 +91,78 @@ if (config.crypto) {
   })
 }
 
-if (!config.crypto)  {
-  var file = fileServer.Server(config.static)
-  server = require('http').createServer(doHttp)
+
+
+function Access (user, db, method, args) {// auth!
+  // if (user && user.name === 'edit') console.log('yessssss!')
+  // if (!user || user.name !== 'vc') console.log('fuk')
 }
 
-function doHttp (req, res) { 
-  file.serve(req, res, function (e, res) {
-    if(e) file.serveFile('/index.html', 200, {}, req, res)
+function Auth (user, cb) {
+  getHmac({token:user.pass,user:user.user}, function handleHmac (d) {
+    if (modes[user.user] === pass) cb(null, { name: user.user, token: d.val })
+    if (modes[user.user] !== pass) cb(new Error('wrong pass!'), null)
+    if (e) cb(e, null)
   })
 }
 
-server.listen(config.port, config.host, function setupWebSocket () {
-  var ws = new webSocketServer({server:server})
+function getHmac (d, cb) { // USERS '@'
+  var hmac = newHmac(ALGO, KEY)
+  hmac.setEncoding('hex')
+  hmac.write(d.token)
+  hmac.end()
+  cb({key:'@:'+d.user, val:hmac.read().toString()})
+}
 
-  ws.on('connection', function newSocketConnection (soc) {
-    var levelServer = multiLevel.server(db)
-    var wss = webSocketStream(soc) 
+// utils!
+function compileComponents (event, file) { // CREATE BETTER LIB '!'
+  fs.readdir(config.components, function (e, files) {
+    var components = {}
+    var b = browserify(config.bundleIn)
 
-    wss.pipe(levelServer).pipe(wss)
-
-    wss.on('error', function (e) {
-      console.error('webSocStreamErr: '+e)
+    files.forEach(function (f) {
+      var name = f.split('.')[0]
+      var ext = f.split('.')[1]
+      if (!components[name]) components[name] = {}
+      var buf = fs.readFileSync(config.components+f).toString()
+      switch (ext) {
+        case 'json' : components[name].pkg = JSON.parse(buf); break;
+        case 'hogan' : components[name].html = buf; break;
+        case 'js' : b.require(config.components+f, {expose: name}); break;
+      }
     })
 
-    brico.installMuxDemux(levelServer)
+    db.put('library', JSON.stringify(components))
+
+    var bun = fs.createWriteStream(config.bundleOut)
+    b.bundle().pipe(bun)
+
+    bun.on('finish', function () {
+      console.log('compiled bundle '+config.bundleIn+' to '+config.bundleOut)
+    })
   })
-})
-
-
-db = level('./'+config.host) 
-liveStream.install(db)
-multiLevel.writeManifest(db, config.static+'manifest.json')
-
-
-var browserBootScript = config.static+'boot.js'
-
-writeBrowserFiles(function thenMappify () { // todo: allow map to be called with cli / web api
-  var dbWriteStream = db.createWriteStream()
-
-  mappify({
-    wilds : config.modules,
-    bundle : config.static+'bundle.js',
-    browserify : browserBootScript,
-    min : config.minify
-  }).pipe(dbWriteStream)
-    .on('error', console.error)
-
-  dbWriteStream.on('close', function () {
-
-    brico = Bricoleur(db, {wilds:config.modules})
-
-    process.stdin.pipe(brico)
-
-    console.log('nimoy running on host: "'+config.host+'" port: "'+config.port+'"')
-    //if (config.cli === true) 
-      // process.stdin.pipe(require('./_cli')()).pipe(brico).pipe(process.stdout)
-  })
-})
-
-
-function writeBrowserFiles (written) {
-
-  var indexHtml = '<!doctype html><html lang="en">'
-    + '<head><meta charset="utf-8"></head>'
-    + '<body><script src="/bundle.js"></script></body>'
-    + '</html>'
-
-  fs.writeFileSync(config.static+'index.html', indexHtml)
-   
-  fs.writeFileSync(browserBootScript, thisFnBodyToString(function () {
-  // Start Browser Boot 
-  var websocStream = require('websocket-stream')
-  var host = window.document.location.host.replace(/:.*/, '')
-  if (window.location.port) host += (':'+window.location.port)
-  if (window.location.protocol === 'https:') var ws = websocStream('wss://' + host)
-  if (window.location.protocol === 'http:') var ws = websocStream('ws://' + host)
-
-  var ml = require('multilevel')
-  var manifest = require('./manifest.json')
-  var multiLevel = ml.client(manifest)
-  var rpc = multiLevel.createRpcStream()
-  ws.pipe(rpc).pipe(ws)
-  ws.on('error', function (e) {
-    console.error(e)
-  })
-
-  var bricoleur = require('../_bricoleur')
-  var Brico = new bricoleur(multiLevel)
-  Brico.installMuxDemux(rpc)
-  // End Browser Boot
-  })) 
-
-  written()
 }
 
+function makeIndex (e,res,d) {
+  if (d) d = JSON.parse(d)
+  if (!d) d = {title:config.title}
 
-// Utils
-function thisFnBodyToString (fn) {// takes fn as input, unwraps and returns string
-  var s = fn.toString()
-  return s.substring(0,s.lastIndexOf('\n')).substring(s.indexOf('\n'),s.length)
+  var indexHTML = fs.readFileSync(config.components+'index.hogan').toString()
+  fs.writeFileSync(index, stache(indexHTML)(d))
 }
+
+function fileUpload(req, res) {
+  var form = new formidable.IncomingForm()
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.write('received upload:\n\n')
+    res.end()
+    var blob = fields.blob.split(',')[1]
+    fs.writeFileSync(config.uploads+fields.file, blob, {encoding:'base64'})
+  })
+}
+
+// compile!
+fs.watch(config.components, compileComponents)
+compileComponents()
+makeIndex()
