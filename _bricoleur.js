a32ee27c1fe268b5640aebd83bd6323afbd0f84f
nimoy <ag@theblacksea.cc>
2014-06-04 11:50:43 -0700

    begin bricoleur refactor

--- _bricoleur.js
+++ _bricoleur.js
@@ -1,135 +1,67 @@
-var interface = require('./_interface')
 var through = require('through')
 
-module.exports = function Bricoleur (multiLevel, opts) { 
+module.exports = function Bricoleur (multiLevel) {
 
-  //  process sets tools
-  var proc = process.title
-  var self = this
-  var MuxDemux
-  var Wilds = {}
-  var _ // duplex stream returned on Bricoleur()
+  var interface = through(function Write (d) { // single filter function
+   
+    // user 
+    
+    // config
+    
+    // lib/index
 
+    if (d.key) { // look at keyspace first
+      var parts = d.key.split(':')
+      var name = parts[0]
 
-  interface.pipe(multiLevel.createWriteStream()) // patch api into db
-  interface.on('error', handleErrors)
-
-  // multiLevel.liveStream({ reverse:true })
-  //   .on('data', signalWilds)
-
-
-  function signalWilds (d) {
-
-    if (!d.type) d.type = 'put' // only applies to old 
-
-    d.key = d.key.split(':')
-
-    var signal = d.key[0]
-
-    switch (signal) {
-
-      case '^' : packages = d.value; break; // library
-
-      case '!' : ; break; // process
-
-      case '*' : makeUnmake(d); break;
-
-      case '|' : pipeUnpipe(d); break;
-
-      case '#' : break; // module data
-
-      case '_' : break; // 
-
-      default : error({}); break;
-
+      switch (keyspace) { // ?
+        case 'config' : break;
+        case 'library' : break;
+        case 'user' : break;
+        default : break;
+      }
     }
-
-  }
-
-
-  function makeUnmake (d) {
-
-    d.value
-      ? Wilds[modName] = require(opts.wilds+name)(d.value) 
-      : Wilds[modName] = require(opts.wilds+name)
-
-    Wilds[modName].id = modName
-
-    // _.emit('data', {
-    //   status:
-    //   body:
-    //   type:
-    // })
-
-  }
-
-
-  function pipeUnpipe (d) {
-
-    mods.map(function getPkgs (uid, i, a) {
-      var name = uid.split('_')[0]
-      var pkg = index[name].nimoy
-      pkg.uid = uid
-      pkg.pos = i
-      a[i] = pkg
-    })
-
-    var s = self.muxDemux.createStream(conn)
-              
-    Wilds[mods[0].uid].pipe(Wilds[mods[1].uid])
-
-    m.pos === 0
-      ? Wilds[m.uid].pipe(s)
-      : s.pipe(Wilds[m.uid])
-
-    // _.emit('data', {
-    //   status:
-    //   body:
-    //   type:
-    // })
-
-  }
-
-
-  function muxPipe (soc) {
-    // make stream module
-    // call pipeUnpipe
-    if (s.meta === hotModule.conn) {
-     (hotModule.pos === 0)
-       ? _[hotModule.uid].pipe(s)
-       : s.pipe(_[hotModule.uid])
-    }
-  }
-
-
-  function error (e) {
-    // inherit from e
-    var err = new Error(e.msg)
-    err.code = e.code
-    _.emit('error', err)
-  }
-
-
-  function handleErrors (e) { // todo
-    console.error(e) 
-  }
-
-
-  _ = through(function Write () {
-    this.emit('data')
-  }, function End () {
-    this.emit('end')
   })
+  
+  var canvas = { // ! to generic
+    put : function (d) { 
+      var keyspace = d.key
+      canvas._[keyspace] = canvas._['render'](moduleName)
 
-  _.installMuxDemux = function (mxdx) {
-    muxDemux = mxdx
-    if (proc === 'browser') muxDemux.on('connection', muxPipe)
+      var connection = d.value.split('>')
+      var a = findModule(connection[0])
+      var b = findModule(connection[1])
+      a.s.pipe(b.s)
+    }, 
+    del : function (d) {
+      var keyspace = d.key
+
+      var connection = d.value.split('>')
+      var a = findModule(connection[0])
+      var b = findModule(connection[1])
+      a.s.unpipe(b.s)
+      
+      canvas._[keyspace].erase()
+      delete canvas._[keyspace]
+    },
+    get : function (d) { // grab key
+      var keyspace = d.key.split(':')
+    },
+    _ : { brico : interface }
   }
 
-  _.write = interface.write.bind(interface)
-  
-  _.wilds = Wilds
+  // branch for keyspace and type
 
-  return _
+  multiLevel.liveStream()
+    .pipe(interface)
 
-} 
+  interface.canvas = canvas
+
+  return interface
+}
+
+function genUID () {// utility functions
+  return new Date().getTime()
+}
+
+ 
