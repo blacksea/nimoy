d9b78898a0b77ca2642de3a2f5080a7b8fd9341b
nimoy <ag@theblacksea.cc>
2014-06-04 11:41:41 -0700

    split nimoy client to sep file, update package and config

---
+++ _client.js
@@ -0,0 +1,27 @@
+var manifest = require('./static/manifest.json')
+
+var db = require('multilevel').client(manifest)
+           .on('error', Errs)
+
+var engine = require('engine.io-stream')
+var ws = engine('/ws')
+ws.pipe(db.createRpcStream()).pipe(ws)
+
+var user = (window.location.host.split('.').length ===  3)
+  ? window.location.host.split('.')[0]
+  : 'view'
+
+var buf = (!window.location.hash) 
+  ? '!'
+  :  window.location.hash.slice(1)
+
+var bricoleur = require('./_bricoleur')(db)
+                  .on('error', Errs)
+        
+bricoleur.write({type:'load', key:'#:'+buf})
+
+window.addEventListener('hashchange', function (e) {
+  bricoleur.write({type:'load', key:'#:'+e.newURL.slice(1)})
+}, false)
+
+function Errs (err) { console.error(err) }
