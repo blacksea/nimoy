3cf211cd52ad39c79b5fd7a6b2e9ce38d733ffc7
nimoy <ag@theblacksea.cc>
2014-06-17 11:03:55 -0700

    mv filter to api

--- _bricoleur.js
+++ _bricoleur.js
@@ -1,52 +1,74 @@
 var through = require('through')
+var conf // arrg globals :(  !!!
 var cvs
+var db
 
-module.exports = function Bricoleur (multiLevel) {
-  var conf = null
+// on load parse / url and build with user session
 
-  function boot (user) {
-    if (user.modules) {
-      user.modules.forEach(function (m) {
+// define session / user abilities 
+  
+var api = {
+  auth : function (d) { // create a new session
+    db.auth({ user:d.user, pass:d.pass }, function (e, res) {
+      if (e) { console.error(e); return false }
+      if (d.origin) cvs._[d.origin].s.write(res); 
+    })
+  }, 
+  session : function (d) {
+    // session pre-loads modules
+    cvs.draw(search(lib, conf.auth))
+    cvs.draw({key:'pipe:'+genUID(), value:conf.auth+'>brico'})
+
+    if (!sessionStorage[user]) {
+      (user === 'default')
+        ? eventStream.emit('data', auth)
+        : C.draw()
+    }
+  },
+  canvas : function (d) {
+    if (d.modules) {
+      d.modules.forEach(function (m) {
         cvs.draw(search(JSON.parse(localStorage.library), m)) 
       })
     }
-    if (user.pipes) {
-      user.pipes.forEach(function (p) {
+    if (d.pipes) {
+      d.pipes.forEach(function (p) {
+        console.log(p)
         cvs.draw({key:'pipe:'+genUID(), value:p})
       })
     }
-  }
-
-  var filter = {}
-
-  filter.library = function (d) {
-    if (!localStorage.library) localStorage.library = d.value
-  }
-
-  filter.config = function (d) {
+  },
+  config : function (d) {
     var lib = JSON.parse(localStorage.library)
     conf = JSON.parse(d.value)
 
-    cvs._.render = require(conf.canvasRender)
-    cvs.draw(search(lib, conf.auth))
-    cvs.draw({key:'pipe:'+genUID(), value:'login>brico'}) // pipes need to use ids
-  }
+    cvs._.render = require(conf.canvasRender) // set render!
 
-  filter.auth = function (d) {
-    multiLevel.auth({user:d.user, pass:d.pass}, function handleAuth (e, res) {
-      if (e) { console.error(e); return false }
-      if (d.origin) { 
-        cvs._[d.origin].s.write(res); 
-        boot(conf.users[d.user])  // needs to be called with user config 
-      }
-    })
+    // install library!
+    
+    // start a new session! 
+
+    
+  },
+  library : function (d) { // load library with config!
+    if (!d.type) d.type = 'put'
+    if (d.type === 'put') {
+      localStorage.library = d.value
+    } else if (d.type === 'get' && d.origin) {
+      cvs._[d.origin].s.write(JSON.parse(localStorage.library))
+    }
   }
-  
+}
+
+module.exports = function Bricoleur (multiLevel) {
+
   var interface = through(function Write (d) {
-    if (!d.key) return
+    if (!d.key) return // ignore if !key property
     var path = d.key.split(':')
-    if (filter[path[0]]) filter[path[0]](d)
+    if (api[path[0]]) api[path[0]](d)
   })
+
+  db = multiLevel
 
   cvs = new Canvas(interface)
 
@@ -58,19 +80,21 @@ module.exports = function Bricoleur (multiLevel) {
   return interface
 }
 
-var Canvas = function (interface) {
+var Canvas = function (interface) { // to save stringify cvs._ to db
   var self = this
 
-  this._ = { brico : { s: interface } }
+  this._ = { brico : { s : interface } }
 
-  this.draw = function (d) { 
+  this.draw = function (d) { // standard object interface!!!
     if (d.nimoy && d.nimoy.module) { // add module to db?
-      d.uid = d.name + '_' + genUID()
+      d.uid = 'module:'+d.name + ':' + genUID()
       self._[d.uid] = self._.render(d)
       return 
     } 
+
     var path = d.key.split(':')
-    if (path[0] === 'pipe') { // add pipe to db?
+
+    if (path[0] === 'pipe') {
       var conn = d.value.split('>')
       var a = search(self._, conn[0])
       var b = search(self._, conn[1])
@@ -80,6 +104,8 @@ var Canvas = function (interface) {
   } 
 
   this.erase = function (d) {
+    if (!d.key) { console.error('CANVAS: bad input',d); return false }
+
     var path = d.key.split(':')
 
     if (path[0] === 'pipe') {
@@ -87,18 +113,20 @@ var Canvas = function (interface) {
       var a = search(self._, conn[0])
       var b = search(self._, conn[1])
       a.unpipe(b)
-
-      delete self._[path]
-
+      delete self._[d.key]
       return
     }
 
-    self._[path].erase()
-    delete self._[path]
+    self._[d.key].erase()
+    delete self._[d.key]
   }
 }
 
-function genUID () { return new Date().getTime() } // UTILS
+// UTILS
+
+function genUID () {
+  return Math.random().toString().slice(2) 
+} 
 
 function search (haystack, needle) { // loop through
   for (hay in haystack) {
