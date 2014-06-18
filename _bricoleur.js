d6a7645c8f42c79f843685e9fa3e6d657c74f3d8
nimoy <ag@theblacksea.cc>
2014-06-18 11:53:02 -0700

    implement basic session

--- _bricoleur.js
+++ _bricoleur.js
@@ -6,10 +6,18 @@ var db
 
 var api = {
   auth : function (d) {
-    db.auth({ user:d.user, pass:d.pass }, function (e, res) {
-      if (e) { console.error(e); return false }
-      if (d.origin) cvs._[d.origin].s.write(res); 
-    })
+    if (d.type === 'put') {
+      db.auth({ user:d.user, pass:d.pass }, function (e, res) {
+        if (e) { console.error(e); return false }
+        // set session!
+        sessionStorage[res.name] = res.token
+        api.canvas(conf.users[user])
+        if (d.origin) cvs._[d.origin].s.write(res)
+      })
+    } else if (d.type === 'del') { 
+      // redraw canvas
+      delete sessionStorage[user] // kill session!
+    }
   }, 
   canvas : function (d) {
     if (d.modules) {
@@ -25,53 +33,32 @@ var api = {
   },
   config : function (d) {
     conf = JSON.parse(d.value)
-
-    localStorage.library = JSON.stringify(conf.library)
-
+    localStorage.library = JSON.stringify(conf.library) // sync on fresh
     cvs._.render = require(conf.canvasRender) // set render!
-    // sync up localStorage with leveldb on put
-
-    // auth ! ?
-    cvs.draw(search(conf.library, conf.auth))
-    cvs.draw({key:'pipe:'+genUID(),value:conf.auth+'>brico'})
-
-    // preboot ! ?
-    api.canvas(conf.users[user])
-
-    if (d.type === 'get') {
-
-      // if no active session do login 
-      cvs.draw(search(lib, conf.auth))
-
-      cvs.draw({key:'pipe:' + genUID(), value: conf.auth + '>brico'})
-
-      if (!sessionStorage[user]) {
-        (user === 'default')
-          ? eventStream.emit('data', auth)
-          : C.draw()
-      }
-    }
-
-    if (d.type === 'put') {
-
-    }
+    
+    if (!sessionStorage[user] && user !== 'default') {
+      cvs.draw(search(conf.library, conf.auth))
+      cvs.draw({key:'pipe:'+genUID(),value:conf.auth+'>brico'})
+    } else {
+      api.canvas(conf.users[user])
+    } 
+    // implement a thorough check to make sure users and tokens match
   }
 }
 
 module.exports = function Bricoleur (multiLevel, usr) {
+  user = usr
+  db = multiLevel
+
   var interface = through(function Write (d) {
     if (!d.key) return // ignore if !key property
 
     var path = d.key.split(':')
+
     if (api[path[0]]) api[path[0]](d)
   })
 
-  db = multiLevel
-
-  user = usr
-  cvs = new Canvas(interface)
-
-  window.cvs = cvs
+  cvs = new Canvas(interface) 
 
   multiLevel.liveStream({reverse : true})
     .pipe(interface)
@@ -97,10 +84,12 @@ var Canvas = function (interface) { // to save stringify cvs._ to db
       var conn = d.value.split('>')
       var a = search(self._, conn[0])
       var b = search(self._, conn[1])
+
       self._[d.key] = d.value
       a.s.pipe(b.s)
     }
   } 
+
   this.erase = function (d) {
     if (!d.key) { console.error('CANVAS: bad input', d); return false }
 
