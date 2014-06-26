e425b6a48b254d7dc5d66e1dbf800ba95652d61c
nimoy <ag@theblacksea.cc>
2014-06-26 15:52:52 -0700

    canvas draw method handles string/array/obj input

--- _bricoleur.js
+++ _bricoleur.js
@@ -1,63 +1,94 @@
-var config = require('./bricoleurConfig.json')
 var hmac = require('crypto-browserify/create-hmac')
+var config = require('./bricoleurConfig.json')
 var Buffer = require('buffer/').Buffer
 var through = require('through')
-
-
-function boot (conf) {
-  var rlib = conf.library.root
-  var glib = conf.library.global
-  localStorage.library = JSON.stringify(glib) 
-
-  cvs._.render = require(conf.canvasRender) // set render!
-  
-  if (!sessionStorage[user] && user !== 'default') {
-    cvs.draw({
-      key: 'module:'+genUID(), 
-      value: search(conf.library.root, conf.auth)
-    })
-    cvs.draw({key:'pipe:'+genUID(), value: conf.auth + '>brico'})
-  } else {
-    api.auth.put({value: {name: user, session: sessionStorage[user]}})
-  }
-}
 
 
 var interface = function (db, cvs, cb) { // API
   self = this
 
-  boot(config)
+  cvs._.render = require(config.canvasRender) // set render!
+  var login = search(config.library.root, config.auth)
+
+  if (sessionStorage[user])
+    self.auth({value: {name: user, session: sessionStorage[user]}})
+
+  if (!sessionStorage[user] && user !== 'default')
+    cvs.draw({key:'pipe:'+genUID(), value: conf.auth + '>brico'})
+
+  cvs.draw([login,'login>brico'])
 
   db.livestream({reverse : true})
     .on('data', sync)
 
-  function sync (d) {
+  function sync (d) { // sync up with db / push updates
     var path = d.key.split(':')
 
+    if (path[0] === 'library') { // install library in this function!
+
+    }
   }
 
-  var s = through(function Write (d) {
-
-    // provide ways to call api in streaming fashion
-    
+  var s = through(function Write (d) { // multiple ways to call api
     console.log(d)
-
   })
 
-  this.deauth = function (d) {
-
-  }
   this.auth = function (d) {
+    var img = new Buffer(config.uImg).toString()
+    console.log(typeof d)
 
+    var auth = { name: d.value.name }
+    if (!d.value.session)
+      auth.pass = hmac('sha256', img).update(d.value.pass).digest('hex')
+
+    if (d.value.session) auth.session = d.value.session
+
+    db.auth(auth, function (e, res) {
+      if (e) { 
+        if (!search(cvs._, conf.auth)) {
+          cvs.draw({
+            key: 'module:'+genUID(), 
+            value: search(conf.library.root, conf.auth)
+          })
+          cvs.draw({key:'pipe:'+genUID(), value: conf.auth + '>brico'})
+        } else console.error(e) // draw login interface!
+        return false
+      }
+      sessionStorage[res.name] = res.token
+      if (conf.users[user].canvas) api.canvas.put(conf.users[user].canvas)
+      if (d.value.origin) cvs._[d.value.origin].s.write(res)
+    })
   }
 
-  // auth !
+  this.deauth = function (d) {
+    db.deauth(function () { 
+      delete sessionStorage[user] 
+      var path = (!getPath()) ? home : home + getPath()
+      window.location = path
+    })
+  }
 
-  // sessions !
+  this.canvas = function (d) {
+    var objects = []
+    if (d.modules) {
+      d.modules.map(function (currentValue, index, array) {
+        var pkg = search(conf.library.root, currentValue) 
+        objects.push({key:'module:'+genUID(), value: pkg})
+      })
+      objects.forEach(cvs.draw)
+    }
+    if (d.pipes) {
+      objects = []
+      d.pipes.forEach(function (p) {
+        objects.push({key:'pipe:'+genUID(), value:p})
+      })
+      objects.forEach(cvs.draw)
+    }
+  }
 
-  // data !
+  this.data = function (d) {
 
-  // canvas !
+  }
 
   cb(s)
 }
@@ -68,19 +99,35 @@ var Canvas = function (interface) { // to save stringify cvs._ to db
   this._ = { brico : { s : interface } }
 
   this.draw = function (d) { 
-    if (!d.key) { console.error('CANVAS: bad input', d); return false }
+    if (typeof d === 'string') { 
+      if (d.split('>').length > 0) { 
+        drawPipe(d.split('>'))
+      } else drawModule(d)
+    }
+    if (typeof d === 'object' && d.nimoy) drawModule(d)
+    if (d instanceof Array) { // sync draw
+      d.forEach(function (str) {
+        if (typeof str === 'string' && str.split('>').length > 0) {
+          drawPipe(str.split('>'))
+        } else drawModule(d)
+      })
+    } 
 
-    var path = d.key.split(':')
+    function drawPipe (conn) {
+      var a = search(self._, conn[0]).s
+      var b = search(self._, conn[1]).s
+      var key = 'pipe:' + conn + ':' + genUID()
+      a.pipe(b)
+      self._[key] = [a , b]
+    }
 
-    if (path[0] === 'pipe') {
-      var conn = d.value.split('>')
-      var a = search(self._, conn[0])
-      var b = search(self._, conn[1])
-      self._[d.key] = d.value
-      a.s.pipe(b.s)
-    } else if (path[0] === 'module') {
-      d.key += ':' + d.value.name
-      self._[d.key] = self._.render(d) // consistent stream protocol
+    function drawModule (nameOrPkg) {
+      var pkg = (typeof nameOrPkg !== 'object') 
+        ? search(config.library.master, nameOrPkg)
+        : nameOrPkg
+      
+      var key = 'module:' + pkg.name + ':' + genUID()
+      self._[key] = self._.render(pkg)
     }
   } 
 
@@ -110,84 +157,9 @@ module.exports = function Bricoleur (multiLevel, usr) {
 }
 
 
-api.auth = {
-  put : function (d) {
-    var img = new Buffer(conf.uImg).toString()
-
-    var auth = { name: d.value.name }
-
-    if (!d.value.session)
-      auth.pass = hmac('sha256', img).update(d.value.pass).digest('hex')
-
-    if (d.value.session) auth.session = d.value.session
-
-    db.auth(auth, function (e, res) {
-      if (e) { 
-        console.error(e)
-        if (!search(cvs._, conf.auth)) {
-          cvs.draw({
-            key: 'module:'+genUID(), 
-            value: search(conf.library.root, conf.auth)
-          })
-          cvs.draw({key:'pipe:'+genUID(), value: conf.auth + '>brico'})
-        } else console.error(e) // draw login interface!
-        return false
-      }
-      sessionStorage[res.name] = res.token
-      if (conf.users[user].canvas) api.canvas.put(conf.users[user].canvas)
-      if (d.value.origin) cvs._[d.value.origin].s.write(res)
-    })
-  }, 
-  del : function (d) {
-    db.deauth(function () { 
-      delete sessionStorage[user] 
-      var path = (!getPath()) ? home : home + getPath()
-      window.location = path
-    })
-  }
-}
-
-api.data = { // fix this to prevent a feedback loop!
-  put : function (d) { 
-    db.put(d.key, d.value, function (e) { if (e) console.error(e) }) 
-  },
-  del : function (d) { db.del(d.key) },
-  get : function (d) { 
-    db.get(d.key, function (e, res) { 
-      if (e) { console.error(e); return false }
-      cvs._[d.key.replace('data','module')].write(res)
-    })
-  }
-}
-
-api.canvas = { // micro macro !?! -- just modifies canvas!
-  put : function (d) { 
-    var objects = []
-    if (d.modules) {
-      d.modules.map(function (currentValue, index, array) {
-        var pkg = search(conf.library.root, currentValue) 
-        objects.push({key:'module:'+genUID(), value: pkg})
-      })
-      objects.forEach(cvs.draw)
-    }
-    if (d.pipes) {
-      objects = []
-      d.pipes.forEach(function (p) {
-        objects.push({key:'pipe:'+genUID(), value:p})
-      })
-      objects.forEach(cvs.draw)
-    }
-  },
-  del : function (d) {
-
-  }
-}
-
 // UTILS
 
-function genUID () {
-  return Math.random().toString().slice(2) 
-} 
+function genUID () { return Math.random().toString().slice(2) } 
 
 function search (haystack, needle) {
   for (hay in haystack) {
