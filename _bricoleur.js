63e0d2e26b8cc01285914e3313cfe036b5b1314e
nimoy <ag@theblacksea.cc>
2014-07-02 15:25:59 -0700

    remove notify from root modules, clean up brico and add api wrapper for cvs.draw/erase as well as import/export

--- _bricoleur.js
+++ _bricoleur.js
@@ -8,32 +8,35 @@ var Canvas = function (interface) {
   var self = this
 
   this._ = { brico : { s : interface } }
+  this.index = { modules : {}, pipes : {} }
 
   function parse (d, cbPipe, cbModule) {
-    if (typeof d === 'string') {  // check for hash!
+    if (typeof d === 'string') { // check for hash!
       (!d.match('>')) ? cbModule(d) : cbPipe(d.split('>'))
-    }
-    if (d instanceof Array) {
-      d.forEach(function (str) {
-        (!str.match('>')) ? cbModule(str) : cbPipe(str.split('>'))
+    } else if (d instanceof Array) {
+      d.forEach(function (item) {
+        (!item.match('>')) ? cbModule(item) : cbPipe(item.split('>'))
       })
-    } 
-    if (typeof d === 'object' && d.nimoy) cbModule(d)
+    } else if (typeof d === 'object' && d.nimoy) cbModule(d)
   }
 
   this.draw = function (d) { 
     parse (d, function drawPipe (conn) {
       var a = search(self._, conn[0])
       var b = search(self._, conn[1])
-      var key = 'pipe:' + genUID(conn) + ':' + conn[0] + '|' + conn[1]
+      var hash = genUID(conn)
+      var key = 'pipe:' + hash + ':' + conn[0] + '|' + conn[1]
       a.s.pipe(b.s)
       self._[key] = [a.id , b.id]
+      self.index.pipes[hash] = [a.id, b.id]
     }, function drawModule (nameOrPkg) {
       var pkg = (typeof nameOrPkg !== 'object') 
         ? search(config.library.master, nameOrPkg)
         : nameOrPkg
-      var key = 'module:' + genUID(pkg.name) + ':' + pkg.name
+      var hash = genUID(pkg.name)
+      var key = 'module:' + hash + ':' + pkg.name
       self._[key] = self._.render({key:key, value:pkg})
+      if (config.library.global[pkg.name]) self.index.modules[hash] = pkg 
     })
   } 
 
@@ -41,47 +44,48 @@ var Canvas = function (interface) {
     parse (d, function erasePipe (pipeID) { 
       var a = self._[pipeID][0] 
       var b = self._[pipeID][1]
-      a.unpipe(b) // not so sure about this...
+      a.unpipe(b) 
       delete self._[pipeID]
+      delete self.index.pipes[pipeID]
     }, function eraseModule (moduleID) {
       var mod = search(self._, moduleID)
-      if (mod) {
-        mod.erase()
-        delete mod
-      }
+      if (mod) { mod.erase(); delete mod }
+      delete self.index.modules[moduleID]
     })
   }
 
-  this.import = function (name) {
-    db.get('canvas:'+name, function (e, res) {
-      if (e) {console.error(e);return}
-      console.log(res)
-
-      // current canvas - imported
-      // in current canvas but not in imported (erase)
-      // in import but not in canvas (draw)
-
-    })
-  }
-
-  this.export = function (name) {
-    var key = 'canvas:'+name
-    var cvsExport = {modules: [], pipes: []}
-    for (item in cvs._) {
-       var  type = item.split(':')[0]
-       var  hash = item.split(':')[1]
-       if (hash && hash.length === 40) { // theres a valid hash
-         if (type==='module') cvsExport.modules.push(hash)
-         if (type==='pipe') cvsExport.pipes.push(hash)
-       }
+  this.import = function (canvas) {
+    console.log(canvas)
+    for (m in self.index.modules) {
+      if (!search(canvas.modules, m)) cvs.erase(m) 
     }
-    db.put(key, cvsExport)
+    for (m in canvas.modules) {
+      if (!search(cvs._, m)) cvs.draw(m)
+    }
+  }
+
+  this.export = function () {
+    var cvsExport = {all: [], modules: {}, pipes: {}}
+    for (item in cvs._) {
+      var  type = item.split(':')[0]
+      var  hash = item.split(':')[1]
+      if (hash && hash.length === 40) {
+        if (type==='module') {
+          var pkg = search(config.library.global, item.split(':')[2])
+          if (pkg) cvsExport.modules[item] = pkg
+        }
+        if (type==='pipe') cvsExport.pipes[item] = cvs._[item]
+      }
+    }
+    return cvsExport
   }
 }
 
 module.exports = function Bricoleur (db, user) {
   localStorage.library = JSON.stringify(config.library.global)
+
   var api = {}
+
   api.auth = function (d) {
     if (!d.value.session) {
       var img = new Buffer(config.uImg).toString()
@@ -90,19 +94,15 @@ module.exports = function Bricoleur (db, user) {
     } else if (d.value.session) {
       db.auth({name: d.value.name, session: d.value.session}, handleAuth)
     }
-
     function handleAuth (e, res) {
       if (e) { 
-        console.error(e)
-        s.push({key:'notify', value: {text:e.message} })
         if (!search(cvs._, config.auth))
           cvs.draw([config.auth, config.auth+'>brico']) 
-      }
-      if (!e) {
+      } else if (!e) {
         sessionStorage[res.name] = res.token
         var login = search(cvs._, config.auth)
         if (typeof login ==='object') cvs.erase(login.id)
-        if (config.users[user].canvas) { // !
+        if (config.users[user].canvas) {
           cvs.draw(config.users[user].canvas.modules)
           cvs.draw(config.users[user].canvas.pipes)
         }
@@ -110,34 +110,40 @@ module.exports = function Bricoleur (db, user) {
       }
     }
   }
+
   api.deauth = function (d) {
-    db.deauth(function () { 
+    db.deauth(function () {
       delete sessionStorage[user] 
       var path = (!getPath()) ? home : home + getPath()
-      window.location = path
     })
   }
+
   api.data = function (d) {
-    if (d.type) {
-      if (d.type === 'put') db.put(d.key,d.value)
-      if (d.type === 'get' && d.origin) {
-        var origin = search(cvs._, d.key.split(':')[1])
-        db.get(d.key, function (e, res) {
-          if (e) {console.error(e); return false}
-          origin.s.write(res)
-        })
-      }
+    if (d.type === 'put') db.put(d.key,d.value)
+    if (d.type === 'get' && d.origin) { // write back to origin
+      var origin = search(cvs._, d.key.split(':')[1])
+      db.get(d.key, function (e, res) {
+        if (e) { console.error(e); return false }
+        origin.s.write(res)
+      })
     }
   }
-  api.module = function (d) {
 
-  }
-  api.pipe = function (d) {
+  api.draw = function (d) { cvs.draw(d.value) }
 
+  api.erase = function (d) { cvs.erase(d.value) }
+
+  api.load = function (d) {
+    var name = d.value
+    db.get('canvas:' + name, function (e, canvas) {
+      if (e) { console.error(e); return false }
+      cvs.import(canvas)
+    })
   }
-  api.canvas = function (d) {
-    // import / export wrapper
-    
+
+  api.save = function (d) {
+    var key = 'canvas:' + d.value
+    db.put(key, cvs.export())
   }
 
   var s = through.obj(function Write (d, enc, next) {
@@ -175,18 +181,15 @@ module.exports = function Bricoleur (db, user) {
 }
 
 // UTILS
-
 function genUID (name) { 
   var r = Math.random().toString().slice(2)
   return hash('sha1').update(name+r).digest('hex')
 } 
-
 function search (haystack, needle) {
   for (hay in haystack) {
     if (hay.match(needle)) return haystack[hay]
   }
 }
-
 function getPath () { 
   if (!window.location.hash) return false
   if (window.location.hash) return window.location.hash.slice(1)
