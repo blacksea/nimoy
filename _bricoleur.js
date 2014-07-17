baacf65b759dbc98b47e89600369001848d816f7
nimoy <ag@theblacksea.cc>
2014-07-17 11:26:51 -0700

    organize brico

--- _bricoleur.js
+++ _bricoleur.js
@@ -1,49 +1,124 @@
 var hmac = require('crypto-browserify/create-hmac')
-var config = require('./bricoleurConfig.json')
 var Buffer = require('buffer/').Buffer
 var through = require('through2')
 var utils = require('utils')
 
-drawPipe = function (conn) {
-  var a = search(self._, conn[0])
-  var b = search(self._, conn[1])
+
+module.exports = function Bricoleur (db, user, config) { //====================
+
+  if (config.canvasRender) var render = require(config.canvasRender)
+
+  var canvas = { index : {} }
+
+  // boot =====================================================================
+  
+  if (sessionStorage[user])
+    api.auth({ value: { name: user, session: sessionStorage[user] } })
+
+  if (!sessionStorage[user] && user !== 'default')
+    api.draw([config.auth, config.auth+'>brico'])
+
+  // end boot =================================================================
+  
+  var interface = through.obj(function Write (d, enc, next) {
+    
+    if (d.key) {
+      var path = d.key.split(':')[0]
+      if (api[path]) api[path](d)
+    }
+
+    function parse (d, cbPipe, cbModule) {
+      if (typeof d === 'string') { 
+        (!d.match('>')) ? cbModule(d) : cbPipe(d.split('>'))
+      } else if (d instanceof Array) {
+        d.forEach(function (item) {
+          (!item.match('>')) ? cbModule(item) : cbPipe(item.split('>'))
+        })
+      } else if (typeof d === 'object' && d.nimoy) cbModule(d)
+    }
+
+    next()
+    put(obj, render)
+
+  })
+
+  db.liveStream({reverse : true})
+    .on('data', sync)
+
+  function sync (d) { 
+    var path = d.key.split(':')[0]
+    if (path === 'file') console.log(d.key) // files!!!
+    if (path === 'data') {
+      var origin = search(canvas._, d.key.split(':')[1])
+      if (origin) origin.s.write(d)
+    }
+  }
+
+  return interface
+
+} // ==========================================================================
+
+
+// BRICOLEUR API ==============================================================
+
+var api = {}
+
+api.get = function (d, cb) {
+  db.get(d.key, function returnResult (e, res) {
+    if (e) cb(e, null)
+    if (!e) cb(null, res)
+  })
+}
+
+api.put = function (d, cb) {
+  var a = utils.search(self._, conn[0])
+  var b = utils.search(self._, conn[1])
   var hash = genUID(conn)
   var key = 'pipe:' + hash + ':' + conn[0] + '|' + conn[1]
+
+  if (!a.pipe || !b.pipe) cb(new Error('unpipable!'), null)
+
   a.pipe(b)
 
-  this._[key] = [a , b]
-  this.index.pipes[hash] = [a.id, b.id]
-}
+  canvas[key] = [a , b]
+  canvas.index.pipes[hash] = [a.id, b.id]
 
-drawModule = function (nameOrPkg) {
-    var pkg = (typeof nameOrPkg !== 'object') 
-      ? search(config.library.master, nameOrPkg)
+  // mod
+
+  var pkg = (typeof nameOrPkg !== 'object') 
+    ? utils.search(config.library.master, nameOrPkg) 
     : nameOrPkg
+
+  if (!pkg) cb(new Error('package not found'), null)
+
   var hash = genUID(pkg.name)
   var key = 'module:' + hash + ':' + pkg.name
-
-  this._[key] = this._.render(pkg, hash)
-  if (config.library.global[pkg.name]) this.index.modules[hash] = pkg 
+  canvas[key] = render(pkg, hash)
+  if (config.library.global[pkg.name]) 
+    canvas.index.modules[hash] = pkg 
 }
 
-erasePipe = function (pipeID) { 
-  var a = this._[pipeID][0] 
-  var b = this._[pipeID][1]
+api.del = function (d, cb) {
+  var a = this._[hash][0] 
+  var b = this._[hash][1]
+
   a.unpipe(b) 
-  delete this._[pipeID]
-  delete this.index.pipes[pipeID]
-}
 
-eraseModule = function (moduleID) {
-  var mod = search(this._, moduleID)
+  delete this._[hash]
+  delete this.index.pipes[hash]
+
+  // mod
+
+  var mod = search(this._, hash)
   if (mod) { 
-    document.body.removeChild(document.getElementById(moduleID))
+    document.body.removeChild(document.getElementById(hash))
     delete mod 
   }
-  delete this.index.modules[moduleID]
+
+  delete this.index.modules[hash]
 }
 
-readCanvas = function (canvasName) {
+api.read = function (canvasName) { 
   for (m in self.index.modules) {
     if (!search(canvas.modules, m)) canvas.erase.module(m) 
   }
@@ -52,7 +127,7 @@ readCanvas = function (canvasName) {
   }
 }
 
-writeCanvas = function (canvasName) {
+api.write = function (canvasName) {
   var cvsExport = {all: [], modules: {}, pipes: {}}
   for (item in canvas._) {
     var  type = item.split(':')[0]
@@ -68,28 +143,19 @@ writeCanvas = function (canvasName) {
   return cvsExport
 }
 
-function parse (d, cbPipe, cbModule) {
-  if (typeof d === 'string') { 
-    (!d.match('>')) ? cbModule(d) : cbPipe(d.split('>'))
-  } else if (d instanceof Array) {
-    d.forEach(function (item) {
-      (!item.match('>')) ? cbModule(item) : cbPipe(item.split('>'))
-    })
-  } else if (typeof d === 'object' && d.nimoy) cbModule(d)
-}
-
-auth = function (d) {
-  if (!d.value.session) {
+api.auth = function (d, cb) {
+  if (!d.session) {
     var img = new Buffer(config.uImg).toString()
     var pass = hmac('sha256', img).update(d.value.pass).digest('hex')
-    db.auth({name: d.value.name, pass: pass}, handleAuth)
-  } else if (d.value.session) {
-    db.auth({name: d.value.name, session: d.value.session}, handleAuth)
+    db.auth({name: d.name, pass: pass}, handleAuth)
+  } else if (d.session) {
+    db.auth({name: d.name, session: d.session}, handleAuth)
   }
+
   function handleAuth (e, res) {
-    if (e) { 
-      if (!search(canvas._, config.auth))
-        api.draw([config.auth, config.auth+'>brico']) 
+    if (e && !search(canvas, config.auth)) { 
+      api.put([config.auth, config.auth+'>brico'])
+      api.draw([config.auth, config.auth+'>brico']) 
     } else if (!e) {
       sessionStorage[res.name] = res.token
       var login = search(canvas._, config.auth)
@@ -103,88 +169,41 @@ auth = function (d) {
   }
 } 
 
-deauth = function () {
+api.deauth = function (d, cb) {
   db.deauth(function () {
     delete sessionStorage[user] 
     var path = (!getPath()) ? home : home + getPath()
   })
 }
 
-data = function (d) {
-  if (d.type === 'put') db.put(d.key,d.value)
-  if (d.type === 'get' && d.origin) {
-    var origin = search(canvas._, d.key.split(':')[1])
-    db.get(d.key, function (e, res) {
-      if (e) { console.error(e); return false }
-      origin.s.write(res)
-    })
-  }
-}
+// login!
+<div class="login">
+  <form id="loginForm">
+    <input type="password" placeholder="enter password" />
+    <input class="send submit" type="submit" value="{{mode}}" style="display:none;"/>
+  </form>
+</div>
 
-load = function (d) {
-  var name = d.value
-  db.get('canvas:' + name, function (e, canvas) {
-    if (e) { console.error(e); return false }
-    canvas.import(canvas)
-  })
-}
+var user = (window.location.host.split('.').length ===  3)
+  ? window.location.host.split('.')[0]
+  : 'default'
 
-save = function (d) {
-  var key = 'canvas:' + d.value
-  db.put(key, canvas.export())
-}
+render(document.body, template.toString(), id)(submit) // render+bind
 
-draw = function (d) { 
-  function parse (d, cbPipe, cbModule) {
-    if (typeof d === 'string') { 
-      (!d.match('>')) ? canvas.draw.module : canvas.draw.pipe
-    } else if (d instanceof Array) {
-      d.forEach(function (item) {
-        (!item.match('>')) ? canvas.draw.module : canvas.draw.pipe
-      })
-    } else if (typeof d === 'object' && d.nimoy) cbModule(d)
-  }
-}
-
-erase = function (d) {
-  canvas.erase
-}
-
-module.exports = function Bricoleur (db, user) {
-  localStorage.library = JSON.stringify(config.library.global)
-
-  var s = through.obj(function Write (d, enc, next) {
-    if (d.key) {
-      var path = d.key.split(':')[0]
-      if (api[path]) api[path](d)
+function submit (el) {
+  var form = el.querySelector('#loginForm')
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    var d = {
+      key : 'auth',
+      value : {
+        origin: id,
+        name: user,
+        pass : e.target[0].value
+      }
     }
-    next()
-  })
-
-  db.liveStream({reverse : true})
-    .on('data', sync)
-
-  function sync (d) { 
-    var path = d.key.split(':')[0]
-    if (path === 'file') console.log(d.key) // files!!!
-    if (path === 'data') {
-      var origin = search(canvas._, d.key.split(':')[1])
-      if (origin) origin.s.write(d)
-    }
-  }
-
-  // boot
-  canvas._.render = require(config.canvasRender) 
-
-  if (sessionStorage[user])
-    api.auth({ value: { name: user, session: sessionStorage[user] } })
-
-  if (!sessionStorage[user] && user !== 'default')
-    api.draw([config.auth, config.auth+'>brico'])
-
-  // end boot
-
-  window.cvs = canvas._
-
-  return s
+    s.push(d)
+  }, false)
 }
+
+
