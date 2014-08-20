5f9d555ddaadf4f01771e588c4e0945eb01f15fa
nimoy <ag@theblacksea.cc>
2014-08-20 10:43:14 -0700

    fix index for new nimoy methods

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// wrapper for nimoy!
+// quickstart wrapper for nimoy!
+
+var nimoy = require('./nimoy')
+var configFlag = process.argv[2] 
+
+var n = !(configFlag) 
+  ? nimoy(require('./config.json'))
+  : nimoy(require(process.argv[2]))
+
+n.compile().boot()
+
+n.on('boot', function () {
+  console.log('nimoy running!')
+})
