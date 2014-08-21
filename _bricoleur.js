9c8a79bb4d8f2709a32468fc56d261d354d02022
nimoy <ag@theblacksea.cc>
2014-08-21 15:05:55 -0700

    consistent return values

--- _bricoleur.js
+++ _bricoleur.js
@@ -16,77 +16,100 @@ module.exports = function Bricoleur (db, user, library) {
       })
     } else parseCommand(d, handleResult)
 
-    function handleResult (e, res) { // return to sender
-      console.log(res)
-      if (!e) { self.push(res) } else { self.emit('error', e) }
+    function handleResult (e, res) { 
+      if (res) {
+        if (d.from) res.key += ':'+d.from
+        self.push(res)
+      }
+      if (e) self.emit('error', e)
       next()
     }
   })
 
   function parseCommand (d, cb) { 
-    var str = (typeof d === 'string') ? d : d.key
-
+    var str = (typeof d === 'string') ? d : d.cmd
+    if (!str) { cb(new Error('bad input!'), null); return false }
     var action = str[0].match(/\+|\-|\?/)[0]
-    var type = str.slice(1).match(/\@|\#|\$|\|/) 
-    type = (!type) ? '*' : type[0] 
-    var actor = (type==='*') ? str.slice(1) : (type==='|') 
+    var type = str.slice(1).match(/\@|\#|\$|\|/);
+    type = (type!==null) ? type[0] : isCuid(str.slice(1)) ? '!' : '*'
+
+    var actor = (type==='!'||type==='*') ? str.slice(1) : (type==='|') 
       ? str.slice(1).split('|') : str.slice(2)
 
     if (!action||!type||!actor) { 
       cb(new Error('wrong cmd:'+str), null); return false
     }
+    var res = {key:type+':'+actor}
 
     // SYMBOLS 
     // ACTIONS: ? get/find, + add, - rm  
     // TYPES: * modules, @ users, # canvas, $ data, | pipes
 
-    if (action==='?') { 
-
+    if (action==='?') { // should also allow for multiple results
       if (type==='*') {
         var pkg = _.find(library, function (v,k) {if (k.match(actor)) return v})
         var uid = _.find(canvas, function (v,k) {
-          if (k.match(actor)) 
-          return k.split(':')[1]
+          if (k.match(actor)) return k.split(':')[1]
         })
-        cb(null, [pkg,uid])
+        res.value = [pkg,uid] 
+        cb(null, res)
         return false
       }
-
       if (type==='#' || type==='$') {
-        var res
-        var s = db.createKeyStream()
-        s.on('data', function (k) {
-          if (k.match(actor)) {
-            res = k
-          }
-        })
-        s.on('end', function () {
-          if (res) cb(null, res)
-          if (!res) cb(new Error('not found: '+actor), null)
-        })
+        db.createKeyStream()
+          .on('data', function (k) {
+            res.value = k
+            if (k.match(actor)) cb(null, res)
+          })
       }
     }
-    
+
+
+    if (type==='!') { // cuid
+      if (action==='-') {
+        var key = _.find(canvas, function (v,k) {if (k.match(actor)) return k})
+        res.value = key
+        if (key && canvas[key]) { delete canvas[key]; cb(null, res) }
+        else cb(new Error(actor+' not found'), null)
+      }
+    }
+
 
     if (type==='|') { 
-      var id = cuid()
-      var modA = actor[0]
-      var modB = actor[1]
+      console.log(actor)
+      if (isCuid(actor[0])===false || isCuid(actor[1])===false) {
+        cb(new Error('unpipeable',null))
+      } else {
+        // check if cuid!
+        var modA = _.find(canvas,function (v,k) {if(k.match(actor[0])) return v})
+        var modB = _.find(canvas,function (v,k) {if(k.match(actor[1])) return v})
 
-      if (action==='+') modA.pipe(modB)
+        if (action==='+') {
+          var id = cuid()
+          modA.pipe(modB)
+          canvas[id] = 'action'
+          res.value = id
+          cb(null, res)
+        }
 
-      if (action==='-') modA.unpipe(modB)
-
-      canvas[id] = 'action'
-      cb(id)
+        if (action==='-') {
+          var modA = canvas[actor][0]
+          var modB = canvas[actor][0]
+          modA.unpipe(modB)
+          delete canvas[actor]
+          res.value = actor
+          cb(null, res)
+        }
+      }
     } 
 
 
     if (type==='*') {
 
       if (action==='-') {
+        res.value = actor
         if (!canvas[actor]) cb(new Error('no module: '+actor),null)
-          else  { delete canvas[actor]; cb(null, actor) }
+          else  { delete canvas[actor]; cb(null, res)
         return false
       }
 
@@ -98,22 +121,30 @@ module.exports = function Bricoleur (db, user, library) {
           cb(new Error('module: '+modName+' not found!'), null); return false  
         }
         canvas[id] = require(pkg.name)(id)
-        cb(null, id)
+        res.value = id
+        cb(null, res)
       }
     }
 
 
     if (type==='$' || type==='#') { 
       var key = type+actor
-      if (action==='+') db.put(key, d.value, cb)
-      if (action==='-') db.del(key, cb)
+      res.value = key
+      if (action==='+') db.put(key, d.value, function (e) {
+        if (e) cb (e, null)
+        if (!e) cb(null, res)
+      })
+      if (action==='-') db.del(key, function (e) {
+        if (e) cb (e, null)
+        if (!e) cb(null, res)
+      })
     }
 
 
     if (type==='@') { 
-
       if (action==='-') {
-        db.deauth(cb)
+        res.value = actor
+        db.deauth(function (e) {cb(null, res)})
         return false
       }
       var auth = actor.split(' ')
@@ -138,7 +169,7 @@ module.exports = function Bricoleur (db, user, library) {
 } 
 
 function isCuid (id) {
-  var r = (typeof id==='string' && id.length===25 && id[0]===0) ? true : false
+  var r = (typeof id==='string' && id.length===25 && id[0]==='c') ? true : false
   return r
 }
 
