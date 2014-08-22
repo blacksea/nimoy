f29db6888ca64e3f7555637c21f1e34f8e70b21b
nimoy <ag@theblacksea.cc>
2014-08-22 16:14:50 -0700

    start script works

--- nimoy.js
+++ nimoy.js
@@ -1,14 +1,18 @@
 // quickstart wrapper for nimoy!
 
-var nimoy = require('./nimoy')
+var nimoy = require('./_nimoy')
 var configFlag = process.argv[2] 
 
-var n = !(configFlag) 
-  ? nimoy(require('./config.json'))
-  : nimoy(require(process.argv[2]))
+var conf = !(configFlag) 
+  ? require('./config.json')
+  : require(process.argv[2])
 
-n.compile().boot()
-
-n.on('boot', function () {
-  console.log('nimoy running!')
+nimoy.compile(conf, function (lib) {
+  require('fs').writeFileSync(conf.path_static+'/library.json')
+  console.log('wrote lib to '+conf.path_static+'/library.json')
 })
+
+nimoy.boot(conf, function () {
+  console.log('Nimoy running on p: '+conf.port+' h: '+conf.host)
+})
+
