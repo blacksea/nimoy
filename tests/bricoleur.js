7ca0d6d204781bef82ff9399aae1713454002efc
nimoy <ag@theblacksea.cc>
2014-08-22 09:18:29 -0700

    simplify bricoleur testss

--- tests/bricoleur.js
+++ tests/bricoleur.js
@@ -1,3 +1,4 @@
+var _ = require('underscore')
 var test = require('tape')
 var level = require('level')
 var multilevel = require('multilevel')
@@ -18,7 +19,7 @@ var server = multilevel.server(db, {
     if (!user.token && pass===user.pass) {
       var sess = cuid()
       sessions[user.name].push(sess)
-      cb(null, {value:sess})
+      cb(null, {key:'@'+user.name,value:sess})
       return null
     } else if (user.token) {
       var exists = _.find(session[user.name],function (s) {
@@ -34,72 +35,74 @@ var client = multilevel.client(require('../static/manifest.json'))
 server.pipe(client.createRpcStream()).pipe(server)
 
 var library = require('../library.json')
-
-library['mod1'] = {
-  name : './tests/mod1'
-}
-
-library['mod2'] = {
-  name : './tests/mod2'
-}
+library['mod1'] = { name : './tests/mod1' }
+library['mod2'] = { name : './tests/mod2' }
 
 var brico = require('../_bricoleur.js')(client,'edit',library)
               .on('error', console.error)
 
 var cmds = [
   {cmd:'+@edit nimoy', from:'auth'},
-  {cmd:'+mod1', from:'add-mod1'},
-  {cmd:'+mod2', from:'add-mod2'},
-  {cmd:'?mod2', from:'find-mod2'}
+  {cmd:'+mod1', from:'addMod1'},
+  {cmd:'+mod2', from:'addMod2'},
+  {cmd:'?mod2', from:'findMod2'}
 ]
 
 test('TEST BRICOLEUR', function (t) {
-  t.plan(9)
-
   var pipe, m1, m2
+
+  var tests = {
+    auth : function (d) {
+      t.equal(d.value,sessions['edit'][0], 'auth succesfull')
+    },
+    addMod1 : function (d) {
+      t.equal(isCuid(d.value), true, 'placed module1')
+      m1 = d.value
+    },
+    addMod2 : function (d) {
+      t.equal(isCuid(d.value), true, 'placed module2')
+      m2 = d.value
+    },
+    findMod2 : function (d) {
+      t.equal(d.value instanceof Array, true, 'search complete')
+      brico.write({cmd:'+'+m1+'|'+m2,from:'pipe'})
+    },
+    pipe: function (d) {
+      pipe = d.value
+      t.equal(isCuid(pipe),true, 'piped modules')
+      brico.write({cmd:'+#cvs',from:'save'})
+    },
+    save: function (d) {
+      t.equal(d.value,'#:cvs', 'saved')
+      brico.write({cmd:'!#cvs',from:'load'})
+    },
+    load: function (d) {
+      t.equal(d.value,'#:cvs', 'loaded')
+      brico.write({cmd:'-'+pipe,from:'unpipe'})
+    },
+    unpipe: function (d) {
+      t.equal(d.value,pipe, 'unpiped modules')
+      brico.write({cmd:'-@edit',from:'logout'})
+    },
+    logout: function (d) {
+      t.equal(d.value, 'edit', 'logged out')
+    }
+  }
+
+  t.plan(_.keys(tests).length)
 
   cmds.forEach(function (c) {
     brico.write(c)
   })
 
   brico.on('data', function (d) {
-    if (d.key.match(cmds[0].from)) {
-      t.equal(d.value,sessions['edit'][0], 'auth succesfull')
-    }
-    if (d.key.match(cmds[1].from)) {
-      t.equal(isCuid(d.value), true, 'placed module1')
-      m1 = d.value
-    }
-    if (d.key.match(cmds[2].from)) {
-      t.equal(isCuid(d.value), true, 'placed module2')
-      m2 = d.value
-    }
-    if (d.key.match(cmds[3].from)) {
-      t.equal(d.value instanceof Array, true, 'search complete')
-      brico.write({cmd:'+'+m1+'|'+m2,from:'pipe'})
-    }
-    if (d.key.slice(2) === 'pipe') {
-      pipe = d.value
-      t.equal(isCuid(pipe),true, 'piped modules')
-      brico.write({cmd:'+#cvs',from:'save'})
-    }
-    if (d.key.match('save')) {
-      t.equal(d.value,'#:cvs', 'saved')
-      brico.write({cmd:'!#cvs',from:'load'})
-    }
-    if (d.key.match('load')) {
-      t.equal(d.value,'#:cvs', 'loaded')
-      brico.write({cmd:'-'+pipe,from:'unpipe'})
-    }
-    if (d.key.match('unpipe')) {
-      t.equal(d.value,pipe, 'unpiped modules')
-      brico.write({cmd:'-@edit',from:'logout'})
-    }
-    if (d.key.match('logout')) {
-      t.equal(d.value, 'edit', 'logged out')
-    }
+    var path = d.key.split(':')
+    var k = path[path.length-1]
+    tests[k](d)
   })
+
 })
+
 
 function isCuid (id) {
   var r = (typeof id==='string' && id.length===25 && id[0]==='c') ? true : false
