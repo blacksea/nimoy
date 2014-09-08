10def53d4fbae0a20b18687f3118d8407cb2a183
nimoy <ag@theblacksea.cc>
2014-09-08 13:54:36 -0700

    load omni if edit mode

--- _client.js
+++ _client.js
@@ -14,10 +14,16 @@ ws.pipe(db.createRpcStream()).pipe(ws)
 var loc = url.parse(window.location.origin)
 
 var user = (loc.host.split('.').length>1) ? loc.host.split('.')[0] : 'unuser'
+
 var canvas = (loc.hash) ? loc.hash : 'home'
 
 var bricoleur = require('./_bricoleur')(db, user, lib)
                   .on('error', Errs)
+
+if (user==='edit') {
+  var omni = require('./lib/omni.js')({id:cuid()})
+  omni.pipe(bricoleur).pipe(omni)
+}
                   
 window.addEventListener('hashchange', function (e) {
   e.preventDefault()
@@ -25,4 +31,8 @@ window.addEventListener('hashchange', function (e) {
   bricoleur.write('!'+canvas)
 }, false)
 
-function Errs (err) { console.error(err) }
+function Errs (err) { 
+  if (err.code===1) { // failed auth!
+    if (omni.write) omni.write({code:1})
+  }
+}
