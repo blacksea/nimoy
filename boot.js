55d7088cedcee2f1bdf2096eec441627ac7ece65
nimoy <ag@theblacksea.cc>
2014-09-14 11:48:54 -0700

    clean up

--- boot.js
+++ boot.js
@@ -1,13 +1,10 @@
-// quickstart wrapper for nimoy!
-
-// allow for ws or engine-io to handle sockets
-
 var fs = require('fs')
 var nimoy = require('./_nimoy')
 var configFlag = process.argv[2] 
 var exec = require('child_process').exec
 var timeModified = ''
 
+// fix this cursed beast!
 
 var conf = !(configFlag) 
   ? require('./config.json')
@@ -18,34 +15,18 @@ if (conf.watch) {
 }
 
 function libDirChange (e,f) {
-  if (e==='change') {
+  if (e === 'change') {
     var ctime = fs.statSync(__dirname+'/lib/'+f).ctime.toString()
     if (timeModified !== ctime) {
-      nimoy.compile(conf, writeLib)
+      nimoy.compile(conf, streamBundle)
       timeModified = ctime
     }
   }
 }
 
-function streamBundle (e,s) {
-  process.stdout.write('200')
-  process.nextTick(function () {
-    var bundleFile = __dirname+'/'+conf.path_static+'/bundle.js'
-    var bundleStream = fs.createWriteStream(bundleFile)
-
-    if (conf.bundle && conf.bundle === 'stream') 
-      s.pipe(process.stdout)
-
-    if (!conf.bundle || conf.bundle === 'file')
-      s.pipe(bundleStream)
-
-    s.on('end',function () {
-      process.stdout.write('X0X')
-      nimoy.boot(conf, function (kill) {
-        process.stdout.write('000')
-      })
-    })
+nimoy.boot(conf, function (kill) {
+  process.stdout.write('000')
+  nimoy.compile(conf, function () {
+    process.stdout.write('200')
   })
-}
-
-nimoy.compile(conf, streamBundle)
+})
