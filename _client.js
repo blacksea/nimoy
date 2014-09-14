5559488b69193c2bb83b53f8e940247a63f0cc49
nimoy <ag@theblacksea.cc>
2014-09-14 11:48:01 -0700

    allow websoc backend to be set in config

--- _client.js
+++ _client.js
@@ -3,44 +3,55 @@ var lib = require('./library.json')
 var url = require('url')
 var cuid = require('cuid')
 var bindshim = require('bindshim')
+var wss = require('websocket-stream')
+var omni
 
-// read config for socket connection / engine
-var ws = (lib.env.soc==='ws') 
+var ws = (!lib.env.soc || lib.env.soc === 'ws') 
   ? require('websocket-stream')('ws://'+lib.env.host+':'+lib.env.port)
   : require('engine.io-stream')('/ws')
+
+ws.on('error', Errs)
 
 var db = require('multilevel').client(manifest)
            .on('error', Errs)
 
-ws.pipe(db.createRpcStream()).pipe(ws)
-
-var loc = url.parse(window.location.origin)
-
-var user = (loc.host.split('.').length>1) ? loc.host.split('.')[0] : 'unuser'
-
-var canvas = (loc.hash) ? loc.hash : '#home'
-
-var bricoleur = require('./_bricoleur')(db, user, lib)
+var bricoleur = require('./_bricoleur')(db,lib)
                   .on('error', Errs)
 
-if (user==='edit') {
-  var omni = require('./lib/omni.js')({id:cuid()})
-  omni.pipe(bricoleur).pipe(omni)
-}
-                  
-window.addEventListener('hashchange', function (e) {
-  e.preventDefault()
-  loc = url.parse(e.newURL)
-  canvas = (loc.hash) ? loc.hash : '#home'
+ws.pipe(db.createRpcStream()).pipe(ws)
+
+function parseUrl (URL) {
+  if (URL.preventDefault && URL.newURL) {
+    URL.preventDefault()
+    URL = URL.newURL
+  }
+
+  var canvas
+  var loc = url.parse(URL)
+  var hash = (loc.hash) ? loc.hash : null
+
+  if (hash && hash.slice(1) === '@') {
+    omni = require('./lib/omni.js')({id:cuid()})
+    omni.pipe(bricoleur).pipe(omni)
+  } else if (hash && hash.slice(1) !== '@') {
+    canvas = hash
+  } else if (!hash) {
+    canvas =  '#home'
+  }
+
   bricoleur.write('!'+canvas)
-}, false)
+}
 
 function Errs (err) {
+  console.error(err)
   if (err.code===1) {
     if (omni.write) omni.write({code:1})
   }
 }
 
-bricoleur.write('!'+canvas)
+bricoleur.on('data', function (d) {
+  console.log(d)
+})
 
-ws.on('error', Errs)
+parseUrl(window.location.origin)
+window.addEventListener('hashchange', parseUrl, false)
