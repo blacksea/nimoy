72520ba73b82cdf940aa3f6d50b26a18d5564dcf
nimoy <ag@theblacksea.cc>
2014-09-16 17:34:23 -0700

    test harness uses real server conns

--- tests/harness.js
+++ tests/harness.js
@@ -1,69 +1,71 @@
 var test = require('tape')
+var level = require('level')
 var spawn = require('child_process').spawn
 var exec = require('child_process').exec
 var request = require('request')
 var run = require('browser-run')
 var br = require('browserify')
-var level = require('level')
 var fs = require('fs')
-
 
 var conf = require('./config.json')
 var db = level('../'+conf.host)
 
 var testCommands = ['+test','+test|bricoleur','+bricoleur|test']
 
-db.put('#:tst', JSON.stringify(testCommands), function (e) {
-  db.close()
-  test('BOOT NIMOY', runner) // load in a seperate specific test
+db.put('#:tst', JSON.stringify(testCommands), function (e) { db.close() })
+
+// sequence : 
+//   - server 
+//     - http req
+//     - websoc
+//   - browser
+//     - client script
+//     - bricoluer api commands
+
+var bundle = ''
+var phantom = run()
+var nimoy = spawn('node',['../boot','./tests/config.json'])
+
+phantom.on('data', function (d) {
+  console.log(d)
+  if (d.slice(0,4)==='# ok' || d.slice(0,6) === '# fail') {
+    phantom.stop()
+    nimoy.kill()
+  }
 })
 
-function runner (t) { 
-  var nimoy = spawn('node',['../boot','./tests/config.json'])
-  // var phantom = run()
-  var bundle = ''
+phantom.on('error', function (e) {
+  console.error(e)
+})
 
-  t.on('end', function () {
-    // phantom.stop()
-    nimoy.kill()
-  })
-
-  // phantom.on('data', function (d) { // output to test!
-  //   console.log(d)
-  // })
-
-  function runBrowser () {
-    phantom.write(bundle)
-    phantom.write(";window.addEventListener('load',"
-      + "function(){window.location.hash='tst'},false);")
-    phantom.end()
-  }
-
-  // instead of piping from compile just send a req to server and pipe!
-
-  nimoy.stdout.on('data', function (d) {
-    var md = d.toString().slice(0,3)
-    if (md === '000') {
-      t.ok(md,'server running')
-    } else if (md === '200') {
-      t.ok(md,'piping bundle')
-      t.end()
-      // var bunLoc = 'http://'+conf.host+':'+conf.port+'/bundle.js'
-      // var bun = require('request')(bunLoc)
-      // bun.on('data', function (d) {
-      //   bundle += d
-      // })
-      // bun.on('end', runBrowser)
-    }
-  })
-
-  nimoy.stderr.on('data', function (e) {
-    console.error(e.toString())
-    nimoy.kill()
-    // phantom.stop()
-    // t.end()
-  })
+function runBrowser () {
+  phantom.write(bundle)
+  phantom.write(";window.addEventListener('load',"
+    + "function(){window.location.hash='tst'},false);")
+  phantom.end()
 }
+
+nimoy.stdout.on('data', function (d) {
+  var md = d.toString().slice(0,3)
+  if (md === '000') {
+    // t.ok(md, 'server running')
+  } else if (md === '200') {
+    var bunLoc = 'http://'+conf.host+':'+conf.port+'/bundle.js'
+    var bun = require('request')(bunLoc)
+    bun.on('data', function (d) {
+      bundle += d
+    })
+    bun.on('end', runBrowser)
+  } else { 
+     console.log(d.toString()) 
+  }
+})
+
+Nimoy.stderr.on('data', function (e) {
+  console.error('FAIL',e.toString())
+  nimoy.kill()
+  phantom.stop()
+})
 
 function isCuid (id) {
   var r = (typeof id==='string' && id.length===25 && id[0]==='c') 
