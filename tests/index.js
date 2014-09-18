1e9fecaf20ebd38bd11e7c94217cd71171ea8c5c
nimoy <ag@theblacksea.cc>
2014-09-18 09:35:06 -0700

    use tap spec for pretty test output

--- tests/index.js
+++ tests/index.js
@@ -6,6 +6,7 @@ var request = require('request')
 var run = require('browser-run')
 var br = require('browserify')
 var fs = require('fs')
+var prettyTap = require('tap-spec')()
 
 var conf = require('./config.json')
 var db = level('../'+conf.host)
@@ -16,51 +17,44 @@ var testCommands = ['+test','+test|bricoleur','+bricoleur|test']
 
 db.put('#:tst', JSON.stringify(testCommands), function (e) { db.close() })
 
-test('Nimoy Boot Script', function (t) {
-  t.plan(2)
+var phantom = run()
+phantom.on('error', console.error)
 
-  t.on('end', function () { // run browser tests in phantom
-    var bunLoc = 'http://'+conf.host+':'+conf.port+'/bundle.js'
-    var bun = require('request')(bunLoc)
-    bun.on('data', function (d) {
-      bundle += d
-    })
-    bun.on('end', function startPhantom () {
-      phantom.write(bundle)
-      phantom.write(";window.addEventListener('load',"
-        + "function(){window.location.hash='tst'},false);")
-      phantom.end()
-    })
+function runBrowserTests () {
+  var bunLoc = 'http://'+conf.host+':'+conf.port+'/bundle.js'
+  var bun = require('request')(bunLoc)
+  bun.on('data', function (d) {
+    bundle += d
   })
-
-  var phantom = run()
-
-  var nimoy = spawn('node',['../boot','./tests/config.json'])
-
-  phantom.on('data', function (d) {
-    console.log(d)
-    if (d.slice(0,4) ==='# ok' || d.slice(0,6) === '# fail') {
-      phantom.stop()
-      nimoy.kill()
-    }
+  bun.on('end', function startPhantom () {
+    phantom.write(bundle)
+    phantom.write(";window.addEventListener('load',"
+      + "function(){window.location.hash='tst'},false);")
+    phantom.end()
   })
+}
 
-  phantom.on('error', console.error)
+phantom.pipe(prettyTap).pipe(process.stdout)
 
-  nimoy.stdout.on('data', function (d) {
-    var md = d.toString().slice(0,5)
-    if (md === 'up on') {
-      t.ok(md, 'server runs')
-    } else if (md === 'wrote') {
-      t.ok(md, 'http request')
-    } else console.log(d.toString()) 
-  })
-
-  nimoy.stderr.on('data', function (e) {
-    console.error('FAIL', e.toString())
-    nimoy.kill()
+phantom.on('data', function (d) {
+  if (d.slice(0,4) ==='# ok' || d.slice(0,6) === '# fail') {
     phantom.stop()
-  })
+    nimoy.kill()
+  }
+})
+
+var nimoy = spawn('node',['../boot','./tests/config.json'])
+
+nimoy.stdout.on('data', function (d) {
+  console.log(d.toString()) 
+  var md = d.toString().slice(0,5)
+  if (md === 'wrote') runBrowserTests()
+})
+
+nimoy.stderr.on('data', function (e) {
+  console.error('FAIL', e.toString())
+  nimoy.kill()
+  phantom.stop()
 })
 
 function isCuid (id) {
