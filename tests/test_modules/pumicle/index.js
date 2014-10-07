6ffa4fb9db0dabc76f7d4b0af224e76229887a32
Anthony Gasca <ag@theblacksea.cc>
2014-10-06 19:57:57 -0700

    update pumicle to use data stream

--- tests/test_modules/pumicle/index.js
+++ tests/test_modules/pumicle/index.js
@@ -1,11 +1,17 @@
 var through = require('through2')
 
-module.exports = function pumicle (opts) {
+module.exports = function pumicle (opts, $) {
+  $.on('data', function (d) {
+  })
 
-  var s = through.obj(function (d, enc, next) {
-    console.log(d)
+  var s = through.obj(function (data, enc, next) {
     next()
   })
 
-  return s
+  setTimeout(function () {
+    var d = {x:90}
+    $.write(d)
+  },1)
+
+  return {s:s, $:$}
 }
