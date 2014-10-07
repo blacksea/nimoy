46bbe1b875b160b1a42463b597025ccfc3cdf838
Anthony Gasca <ag@theblacksea.cc>
2014-10-06 19:57:37 -0700

    update tests, make adding new api tests easier

--- tests/test_modules/test/index.js
+++ tests/test_modules/test/index.js
@@ -4,68 +4,57 @@ var _ = require('underscore')
 var test = require('tape')
 var cuid = require('cuid')
 
-sim.contextMenu = function (el) { // contextmenu monkeypatch
+
+// contextmenu monkeypatch //////////////////////////////////////////////////
+sim.contextMenu = function (el) { 
   var e = document.createEvent('MouseEvents')
   e.initMouseEvent(
-    'contextmenu',true,true,window,1,0,0,0,0,false,false,false,false,1,null
-  )
+    'contextmenu',true,true,window,1,0,0,0,0,false,false,false,false,1,null)
   el.dispatchEvent(e)
-}
+} ///////////////////////////////////////////////////////////////////////////
 
-var cmds = [
-  ['+@edit nimoy', cuid()],
-  ['+gooshter', cuid()],
-  ['+pumicle', cuid()],
-  ['+gooshter|pumicle', cuid()],
-  ['-gooshter|pumicle', cuid()],
-  ['-gooshter', cuid()],
-  ['-pumicle', cuid()],
-  ['-@edit', cuid()]
-]
 
-module.exports = function ClientTest (opts) {
-  var p = through.obj()
-  var s = through.obj(function (d,enc,next) { if (!done) p.write(d); next()})
+// bricoleur api tests (directly using stream interface)
+
+var cmds = {}
+cmds[cuid()] = ['+@edit nimoy', function(t,d){ t.ok(d.value, '+@edit') }]
+cmds[cuid()] = ['+gooshter', function (t,d) { t.ok(d.value, '+gooshter')}]
+cmds[cuid()] = ['+pumicle', function (t,d) { t.ok(d.value, '+pumicle')}]
+cmds[cuid()] = ['+gooshter|pumicle', function (t,d) {t.ok(d.value,'+pumicle')}]
+cmds[cuid()] = ['-gooshter|pumicle', function (t,d) {t.ok(d.value,'+pumicle')}]
+cmds[cuid()] = ['-gooshter', function (t,d) { t.ok(d.value, '+pumicle')}]
+cmds[cuid()] = ['-pumicle', function (t,d) { t.ok(d.value, '+pumicle')}]
+cmds[cuid()] = ['-@edit', function (t,d) { t.ok(d.value, '+pumicle')}]
+
+
+module.exports = function BrowserTest (opts) {
   var done = false
 
-  function getIndex (str, arr) { 
-    var res = null
-    for (var i=0;i<arr.length;i++) {
-      arr[i].forEach(function (item) { if (item === str) res = i })
-    }
-    return res.toString()
-  }
+  var s = through.obj(function (d,e,n) { ps.write(d); n() })
+  var ps = through.obj()
 
   test('bricoleur api', function (t) { 
-    t.plan(cmds.length) 
+    t.plan(8) 
 
-    cmds.forEach(function (c) { s.push(c[0]+':'+c[1]) })
+    for (c in cmds) { s.push(cmds[c][0]+':'+c) }
 
-    p.on('data', function (d) {
-      if (typeof d === 'object' && d.key && d.value) {
-        var i = (d.key.split(':').length>1) 
-          ? getIndex(d.key.split(':')[1], cmds) 
-          : getIndex(d.key, cmds)
-
-        if (typeof i === 'string') i = Math.abs(i)
-        var c = cmds[i]
-        t.ok(d.value, 'do '+c[0])
-      } 
+    ps.on('data', function (d) {
+      if (!d.key || done) return false
+      var k = (d.key.split(':').length > 1) ? d.key.split(':')[1] : d.key
+      if (cmds[k]) cmds[k][1](t,d)
     })
 
-    t.on('end', function () {
-      done = true
-      test('gui interactions', interfaceTest)
-    })
+    t.on('end', function () { done = true; test('gui interactions', domTest) })
   })
 
   return s
 }
 
-function interfaceTest (t) {
-  t.plan(3)
 
-  // test dombii! && possibly drompii!
+// ui/dom tests
+
+function domTest (t) {
+  t.plan(3)
 
   var pumicle = ['?','p','u','m','i','c','l','e']
 
@@ -76,30 +65,31 @@ function interfaceTest (t) {
   function domNodeAdd (e) {
     var target = e.target
 
-    if (target.className==='login') {
-      // simulate bad logins as well!
+    if (target.className==='login') { // simulate bad logins as well!
 
       t.ok(target, 'login drawn')
       target.querySelector('#login').value = 'nimoy'
       process.nextTick(function () { sim.submit(target) })
 
     } else if (target.children[0].className==='omni') {
+
       var input = target.querySelector('input')
-      // process.nextTick(function () { 
-        // typeIt(pumicle, input) 
-        // sim.contextMenu(document.body.querySelector('.mool'))
-      // })
+      process.nextTick(function () { 
+        // typeIt(pumicle, input)
+        // sim.submit(input)
+        // sim.contextMenu(document.body.querySelectorAll('.mool')[2])
+      })
+
       t.ok(target, 'omni drawn')
     }
   }
 
   function domNodeRm (e) {
     var target = e.target
-    if (target.className==='login') t.ok(target,'login erased')
+    if (target.className === 'login') t.ok(target,'login erased')
   }
 
-  window.location.hash = '@' // trigger login
-
+  window.location.hash = '@' 
   document.body.addEventListener("DOMNodeInserted", domNodeAdd, false)
   document.body.addEventListener("DOMNodeRemoved", domNodeRm, false)
 }
