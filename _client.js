7204acff5554a62af9da906a333ce101b09bc1d8
nina <ag@theblacksea.cc>
2014-11-17 12:00:24 -0800

    simplify client load script

--- _client.js
+++ _client.js
@@ -3,36 +3,24 @@ var lib = require('./library.json')
 var url = require('url')
 var cuid = require('cuid')
 var bindshim = require('bindshim')
-var om = require('./lib/omni.js')
-var omni
 
 var ws = (!lib.env.soc || lib.env.soc === 'ws') 
   ? require('websocket-stream')('ws://'+lib.env.host+':'+lib.env.port)
   : null
  
-
 ws.on('error', Errs)
-
 
 var db = require('multilevel').client(manifest)
            .on('error', Errs)
 
+ws.pipe(db.createRpcStream()).pipe(ws)
 
 var bricoleur = require('./_bricoleur')(db,lib)
                   .on('error', Errs)
 
+var omni = require('./lib/omni.js')({id:cuid(),lib:lib})
 
-ws.pipe(db.createRpcStream()).pipe(ws)
-
-
-// enter a metakey to activate omni -- if not logged in show login window
-
-
-function Errs (err) {
-  if (err.code === 1) 
-    if (omni.write) omni.write({code:1})
-}
-
+omni.pipe(bricoleur).pipe(omni)
 
 function loadCanvas (loc) {
   loc = (loc.newURL) ? url.parse(loc.newURL) : url.parse(loc)
@@ -43,27 +31,15 @@ function loadCanvas (loc) {
     ? 'home' 
     : loc.hash.slice(1)
 
-  if (sessionStorage.edit && !document.querySelector('.omni')) {
-    omni = om({id:cuid(),lib:lib})
-    omni.pipe(bricoleur).pipe(omni)
-  } 
-
-  if (canvas[0] !== '@') bricoleur.write('!#'+canvas)
+  bricoleur.write('!#'+canvas)
 }
 
+function Errs (err) {
+  if (err.code === 1) 
+    if (omni.write) omni.write({code:1})
+}
 
 window.addEventListener('hashchange', loadCanvas, false)
-
-
-document.addEventListener('keydown', function initOmni (e) {
-  if (e.keyCode === 13 && !document.querySelector('.omni')) {
-    if (e.metaKey || e.ctrlKey) { 
-      omni = om({id:cuid(),lib:lib})
-      omni.pipe(bricoleur).pipe(omni)
-    }
-  }
-},false)
-
 
 window.addEventListener('popstate', function (e) {
   if (e.state) console.log(e.state)
