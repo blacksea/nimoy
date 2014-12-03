ce22a92e17fac3d03b99df02e46f07ad3534a973
nina <ag@theblacksea.cc>
2014-12-03 10:03:45 -0800

    remove logging

--- tests/test_modules/project/index.js
+++ tests/test_modules/project/index.js
@@ -2,13 +2,17 @@ var through = require('through2')
 var D = require('../../../lib/dombii.js')
 
 module.exports = function ($) {
+  var id = $.meta
 
   var t 
   var s = through.obj()
 
   $.on('data', function (d) { 
-    if (d.nimoy) t = new D(d)
-    else t.draw(d) 
+    if (d.nimoy) {
+      t = new D(d)
+    } else {
+      t.draw(d) 
+    }
   })
 
   s.on('close', function () {t.erase()})
