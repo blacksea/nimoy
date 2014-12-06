f94cf6cf8c49caec1915de8c6754fd58cf4bfecd
nina <ag@theblacksea.cc>
2014-12-05 20:46:29 -0800

    update project to use gallery

--- tests/test_modules/project/index.js
+++ tests/test_modules/project/index.js
@@ -1,22 +1,37 @@
 var through = require('through2')
 var D = require('../../../lib/dombii.js')
+var url = require('url')
 
 module.exports = function ($) {
+  var t, selected, main
   var id = $.meta
-
-  var t 
   var s = through.obj()
 
   $.on('data', function (d) { 
     if (d.nimoy) {
+      d.events = [['.thumbs a','click', bigify]]
       t = new D(d)
-      t.draw(d.data)
+      d.data.main = d.data.slides[0].src
+      d.data.descript = d.data.slides[0].txt
+      t.draw(d.data,init)
     } else {
-      t.draw(d) 
+      d.main = 'url("'+d.slides[0].src+'")'
+      d.descript = d.slides[0].txt
+      t.draw(d)
     }
   })
 
-  s.on('close', function () {t.erase()})
+  $.on('close', function () { t.erase() })
+
+  function init () {main = document.getElementById(id).querySelector('.main')}
+
+  function bigify (e) {
+    selected = e.target
+    var sBG = selected.style.backgroundImage.replace(/url\(|\)/g,'')
+    t.data.main = url.parse(sBG).pathname.replace(/\%22/g,'')
+    t.data.descript = selected.title
+    t.draw(t.data)
+  }
 
   return {s:s, $:$}
 }
